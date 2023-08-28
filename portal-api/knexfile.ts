a3ecbf1d96baf67bc0dc4f4f1cdaa3b3c67dcb05
Julien Richard <julien.richard@filigran.io>
2023-08-28 03:17:16 +0200

    Trying generic Node api

--- portal-api/knexfile.ts
+++ portal-api/knexfile.ts
@@ -1,8 +1,9 @@
 import portalConfig from "./src/config.js";
-import pkg from "knex";
+import pkg, { Knex } from "knex";
+import {toGlobalId} from "graphql-relay/node/node.js";
 const { knex } = pkg;
 
-const config = {
+const config: Knex.Config = {
     client: 'pg',
     connection: {
         host: portalConfig.database.host,
@@ -20,9 +21,22 @@ const config = {
         extension: 'js',
         directory: 'src/seeds',
     },
+    postProcessResponse: (result, queryContext) => {
+        if(!queryContext?.__typename) return result;
+        const __typename = queryContext.__typename;
+        if (Array.isArray(result)) {
+            return result.map(row => ({ ...row, id: toGlobalId(__typename, row.id), __typename }));
+        } else {
+            return { ...result, id: toGlobalId(__typename, result.id), __typename };
+        }
+    }
 };
 
 export const database = knex(config);
+
+export const dbFrom = <T>(type: string) => {
+    return database<T>(type).queryContext({ __typename: type })
+}
 
 // noinspection JSUnusedGlobalSymbols
 export default config;
--- portal-api/src/migrations/20230827150733_init.js
+++ portal-api/src/migrations/20230827150733_init.js
@@ -1,34 +1,37 @@
 export async function up(knex) {
+    // Ensure UUID extension is activated
+    await knex.schema.raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"');
+
     // Create table organization
-    await knex.schema.createTable('organizations', (table) => {
-        table.string('id').primary();
+    await knex.schema.createTable('Organization', (table) => {
+        table.uuid('id', { primaryKey: true });
         table.string('name');
         table.unique('name');
     });
 
     // Create table user
-    await knex.schema.createTable('users', (table) => {
-        table.string('id').primary();
+    await knex.schema.createTable('User', (table) => {
+        table.uuid('id', { primaryKey: true });
         table.string('email');
         table.unique('email');
         table.string('salt').notNullable();
         table.string('password').notNullable();
         table.string('first_name');
         table.string('last_name');
-        table.string('organization_id').notNullable();
-        table.foreign('organization_id').references('id').inTable('organizations');
+        table.uuid('organization_id').notNullable();
+        table.foreign('organization_id').references('id').inTable('Organization');
     });
 
     // Create table deployment
-    await knex.schema.createTable('services', (table) => {
-        table.string('id').primary();
+    await knex.schema.createTable('Service', (table) => {
+        table.uuid('id', { primaryKey: true });
         table.string('name').notNullable();
         table.string('description');
     });
 
     // Create table subscription
     // await knex.schema.createTable('subscriptions', (table) => {
-    //     table.string('id').primary();
+    //     table.uuid('id', { primaryKey: true });
     //     table.string('name').notNullable();
     //     table.string('description');
     //     table.string('service').notNullable();
@@ -37,8 +40,9 @@ export async function up(knex) {
 }
 
 export async function down(knex) {
-    await knex.schema.dropTable('organizations');
-    await knex.schema.dropTable('users');
-    await knex.schema.dropTable('services');
+    await knex.schema.raw('DROP EXTENSION "uuid-ossp"');
+    await knex.schema.dropTable('Organization');
+    await knex.schema.dropTable('User');
+    await knex.schema.dropTable('Service');
 }
 
