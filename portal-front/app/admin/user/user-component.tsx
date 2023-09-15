e198f2a5819486cab6fff002edb078474265573e
Julien Richard <julien.richard@filigran.io>
2023-09-15 23:34:50 +0200

    Adapt data fragment

--- portal-front/app/admin/user/user-component.tsx
+++ portal-front/app/admin/user/user-component.tsx
@@ -1,31 +1,25 @@
 import {PreloadedQuery, usePaginationFragment, usePreloadedQuery} from "react-relay";
-import {userQuery} from "../../../__generated__/userQuery.graphql";
+import {userPreloaderQuery} from "../../../__generated__/userPreloaderQuery.graphql";
 import * as React from "react";
 import {serviceQuery} from "../../../__generated__/serviceQuery.graphql";
 import {UserQuery, usersFragment} from "./user-preloader";
-import {user_users$key} from "../../../__generated__/user_users.graphql";
+import {userPreloader_users$key} from "../../../__generated__/userPreloader_users.graphql";
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 
 interface ServiceProps {
-    queryRef: PreloadedQuery<userQuery>
+    queryRef: PreloadedQuery<userPreloaderQuery>
 }
 
 const UserComponent: React.FunctionComponent<ServiceProps> = ({queryRef}) => {
-    const queryData = usePreloadedQuery<userQuery>(UserQuery, queryRef);
-    const {data} = usePaginationFragment<serviceQuery, user_users$key>(usersFragment, queryData);
+    const queryData = usePreloadedQuery<userPreloaderQuery>(UserQuery, queryRef);
+    const {data} = usePaginationFragment<serviceQuery, userPreloader_users$key>(usersFragment, queryData);
     return <TableContainer component={Paper}>
-        <Table sx={{minWidth: 650}} aria-label="simple table">
-            <TableHead>
-                <TableRow>
-                    <TableCell>Email</TableCell>
-                </TableRow>
-            </TableHead>
+        <Table sx={{minWidth: 650}} aria-label="custom pagination table">
             <TableBody>
                 {data.users.edges.map((user) => (
                     <TableRow key={user.node?.email} sx={{'&:last-child td, &:last-child th': {border: 0}}}>
