016d706783bc4ef5606722387dbe6cdd1aca79d0
Julien Richard <julien.richard@filigran.io>
2023-09-15 23:13:03 +0200

    Adapt queries from file change

--- portal-front/app/admin/user/page.tsx
+++ portal-front/app/admin/user/page.tsx
@@ -1,12 +1,12 @@
 import * as React from "react";
 import loadSerializableQuery from "@/relay/loadSerializableQuery";
-import userQueryNode, {userQuery} from "../../../__generated__/userQuery.graphql";
+import userPreloaderQueryNode, {userPreloaderQuery} from "../../../__generated__/userPreloaderQuery.graphql";
 import UserPreloader from "./user-preloader";
 
 const DEFAULT_COUNT = 10;
 
 const Page = async () => {
-    const preloadedQuery = await loadSerializableQuery<typeof userQueryNode, userQuery>(userQueryNode, {
+    const preloadedQuery = await loadSerializableQuery<typeof userPreloaderQueryNode,userPreloaderQuery>(userPreloaderQueryNode, {
         count: DEFAULT_COUNT,
         orderBy: "email",
         orderMode: "asc"
--- portal-front/app/admin/user/user-preloader.tsx
+++ portal-front/app/admin/user/user-preloader.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import {SerializablePreloadedQuery} from "@/relay/loadSerializableQuery";
-import userQueryNode, {userQuery} from "../../../__generated__/userQuery.graphql";
+import userPreloaderQueryNode, {userPreloaderQuery} from "../../../__generated__/userPreloaderQuery.graphql";
 import * as React from "react";
 import useSerializablePreloadedQuery from "@/hooks/useSerializablePreloadedQuery";
 import {graphql} from "react-relay";
@@ -9,7 +9,7 @@ import UserComponent from "./user-component";
 
 // region queries and fragments
 export const usersFragment = graphql`
-    fragment user_users on Query
+    fragment userPreloader_users on Query
     @refetchable(queryName: "UsersPaginationQuery") {
         users(first: $count, after: $cursor, orderBy: $orderBy, orderMode: $orderMode) @connection(key: "Admin_users") {
             __id # See https://relay.dev/docs/guided-tour/list-data/updating-connections/#using-declarative-directives
@@ -23,13 +23,13 @@ export const usersFragment = graphql`
     }
 `;
 export const UserQuery = graphql`
-    query userQuery($count: Int!, $cursor: ID, $orderBy: UserOrdering!, $orderMode: OrderingMode!) {
-        ...user_users
+    query userPreloaderQuery($count: Int!, $cursor: ID, $orderBy: UserOrdering!, $orderMode: OrderingMode!) {
+        ...userPreloader_users
     }
 `;
 // endregion
 
-const UserPreloader = (props: { preloadedQuery: SerializablePreloadedQuery<typeof userQueryNode, userQuery> }) => {
+const UserPreloader = (props: { preloadedQuery: SerializablePreloadedQuery<typeof userPreloaderQueryNode, userPreloaderQuery> }) => {
     const queryRef = useSerializablePreloadedQuery(props.preloadedQuery);
     return <UserComponent queryRef={queryRef}/>
 };
