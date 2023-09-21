2a8488a4ab8b945f0793c51c01daf3058f7230ac
Julien Richard <julien.richard@filigran.io>
2023-09-21 09:47:20 +0200

    Simplify authentication directives

--- portal-api/src/directives/auth.ts
+++ portal-api/src/directives/auth.ts
@@ -3,8 +3,7 @@ import {defaultFieldResolver, GraphQLSchema} from "graphql";
 import type {PortalContext, User} from "../index.js";
 import {CAPABILITY_BYPASS} from "../../knexfile.js";
 
-export const AUTH_DIRECTIVE_NAME = "authenticated";
-export const ROLES_DIRECTIVE_NAME = "hasRole";
+export const AUTH_DIRECTIVE_NAME = "auth";
 
 type AuthFn = (user: User) => boolean;
 type RoleFn = (user: User, roleRequiredInSchema: string[]) => boolean;
@@ -14,31 +13,24 @@ const getSchemaTransformer = (isAuthenticatedFn: AuthFn, hasRoleFn: RoleFn) => {
         authDirectiveTransformer: (schema: GraphQLSchema): GraphQLSchema =>
             mapSchema(schema, {
                 [MapperKind.TYPE]: type => {
-                    // Save the authn info to the type fields
-                    const authnDirective = getDirective(schema, type, AUTH_DIRECTIVE_NAME)?.[0];
-                    if (authnDirective) {
-                        typeDirectiveArgumentMaps[type.name] = authnDirective;
-                    }
-                    // Save the authz info to the type fields
-                    const authzDirective = getDirective(schema, type, ROLES_DIRECTIVE_NAME)?.[0];
-                    if (authzDirective) {
-                        typeDirectiveArgumentMaps[type.name] = authzDirective;
+                    const authDirective = getDirective(schema, type, AUTH_DIRECTIVE_NAME)?.[0];
+                    if (authDirective) {
+                        typeDirectiveArgumentMaps[type.name] = authDirective;
                     }
                     return undefined;
                 },
                 [MapperKind.OBJECT_FIELD]: (fieldConfig, _fieldName, typeName) => {
-                    const authNDirective = getDirective(schema, fieldConfig, AUTH_DIRECTIVE_NAME)?.[0] ?? typeDirectiveArgumentMaps[typeName];
-                    const authZDirective = getDirective(schema, fieldConfig, ROLES_DIRECTIVE_NAME)?.[0] ?? typeDirectiveArgumentMaps[typeName];
+                    const authDirective = getDirective(schema, fieldConfig, AUTH_DIRECTIVE_NAME)?.[0] ?? typeDirectiveArgumentMaps[typeName];
                     const {resolve = defaultFieldResolver} = fieldConfig;
                     fieldConfig.resolve = function (source, args, context: PortalContext, info) {
                         const {user} = context;
-                        const requiredAuthZ = authZDirective?.requires;
+                        const rolesRequired = authDirective?.requires;
                         // Check if the field requires authentication
-                        if ((authNDirective || requiredAuthZ) && !isAuthenticatedFn(user)) {
+                        if (authDirective && !isAuthenticatedFn(user)) {
                             throw new Error(`Not authenticated.`);
                         }
                         // Get the required authorization role for the requested field
-                        if (requiredAuthZ && !hasRoleFn(user, requiredAuthZ)) {
+                        if (authDirective && !hasRoleFn(user, rolesRequired)) {
                             throw new Error(`Not authorized. The provided role does not meet schema requirements`);
                         }
                         // Else, run the resolvers as normal
@@ -54,13 +46,13 @@ const isAuthenticated = (user: User) => {
     return !!user
 };
 
-const hasRole = (user: User, roleRequiredInSchema: string[]) => {
+const hasRole = (user: User, rolesRequired: string[]) => {
     const {capabilities} = user;
     const capabilityNames = capabilities.map((c) => c.name);
     if (capabilityNames.includes(CAPABILITY_BYPASS.name)) {
         return true;
     }
-    return capabilityNames.some((id) => roleRequiredInSchema.includes(id));
+    return capabilityNames.some((id) => rolesRequired.includes(id));
 };
 
 export const {authDirectiveTransformer} = getSchemaTransformer(isAuthenticated, hasRole);
