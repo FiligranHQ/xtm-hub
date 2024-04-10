de5c7da73edabe9132f9d1d1496c073811a58680
Jean-Philippe Kha <jean-philippe.kha@filigran.io>
2024-04-10 10:34:14 +0200

    [backend] Change config for AWX action

--- portal-api/src/managers/awx/awx-configuration.ts
+++ portal-api/src/managers/awx/awx-configuration.ts
@@ -1,32 +1,34 @@
 import config from 'config';
-import { AWXAction, AWXActionFunctionMap, AwxResponse, AWXWorkflowAction } from './awx.model';
+import { AWXAction, AWXActionFunctionMap, AwxResponse, AWXWorkflowAction, AWXWorkflowConfig } from './awx.model';
 import { AWX_HEADERS, AWX_URL, AWX_WORKFLOW_URL } from './awx.const';
 import { buildCreateUserInput } from './awx-mapping';
 
 export const launchAWXWorkflow = async (action: AWXWorkflowAction) => {
-  const workflow = await awxGetWorkflow(action.type);
-  if (workflow.count > 0) {
-    const workflowId = workflow.results[0].id;
-    return await awxLaunchWorkflowId(workflowId, {
-      'extra_vars': await buildWorkflowInput(action),
-    });
+  const awxWorkflow: AWXWorkflowConfig = config.get(`awx.action_mapping.${action.type}`);
+  if (!awxWorkflow) {
+    throw new Error(`awx.action_mapping.${action.type} is not defined`);
   }
-  throw new Error(`awx.action_mapping.${action.type} is not defined`);
+
+  const workflow = await awxGetWorkflow(awxWorkflow.path);
+
+  return await awxLaunchWorkflowId(workflow, {
+    'extra_vars': await buildWorkflowInput(action, awxWorkflow.keys),
+  });
 };
 
-export const awxGetWorkflow = async (workflowName: AWXAction): Promise<AwxResponse> => {
-  const apiURL = config.get(`awx.action_mapping.${workflowName}`);
-  if (!apiURL) {
-    throw new Error(`awx.action_mapping.${workflowName} is not defined`);
-  }
+export const awxGetWorkflow = async (apiURL: string): Promise<AwxResponse> => {
   const url = `${AWX_URL}${apiURL}`;
   const response = await fetch(url, { headers: AWX_HEADERS });
   return await response.json() as AwxResponse;
 };
 
-export const awxLaunchWorkflowId = async (workflowId: number, body: object) => {
+export const awxLaunchWorkflowId = async (workflow: AwxResponse, body: object) => {
+  if (workflow.count === 0) {
+    throw new Error(`Error we did not find any result`);
+  }
+  const workflowId = workflow.results[0].id;
   const url = AWX_URL + AWX_WORKFLOW_URL.replace('${workflowId}', workflowId.toString(10));
-  const workflow = await fetch(url, {
+  const awxWorkflow = await fetch(url, {
     headers: AWX_HEADERS,
     body: JSON.stringify(body),
     method: 'POST',
@@ -36,13 +38,13 @@ export const awxLaunchWorkflowId = async (workflowId: number, body: object) => {
       return Promise.reject(error);
     },
   );
-  return await workflow.json();
+  return await awxWorkflow.json();
 };
 
-const buildWorkflowInput = async (action: AWXWorkflowAction) => {
+const buildWorkflowInput = async (action: AWXWorkflowAction, keys: string[]) => {
   const workflowInput: AWXActionFunctionMap = {
     [AWXAction.CREATE_USER]: buildCreateUserInput,
     [AWXAction.DISABLE_USER]: buildCreateUserInput,
   };
-  return await workflowInput[action.type](action.input);
+  return await workflowInput[action.type](action.input, keys);
 };
