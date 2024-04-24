fa3c0ad4708c55518f714d7c9761b9ae662aece7
Jean-Philippe Kha <jean-philippe.kha@filigran.io>
2024-04-24 14:35:59 +0200

    [backend] migration - create message tracking and modify action tracking

---
+++ portal-api/src/migrations/20240424065418_create_message_tracking.js
@@ -0,0 +1,40 @@
+import { v4 as uuidv4 } from 'uuid';
+
+/**
+ * @param { import('knex').Knex } knex
+ * @returns { Promise<void> }
+ */
+export async function up(knex) {
+  // Create action tracking table
+  await knex.schema.createTable('MessageTracking', (table) => {
+    table.uuid('id', { primaryKey: true }).notNullable().defaultTo(uuidv4());
+    table.uuid('tracking_id').references('id').inTable('ActionTracking')
+      .onDelete('CASCADE');
+    table.timestamp('created_at').notNullable().defaultTo(knex.fn.now());
+    table.boolean('technical').defaultTo(false);
+    table.string('type').notNullable();
+    table.json('tracking_info');
+  });
+
+  await knex.schema.table('ActionTracking', function(table) {
+    table.dropColumn('type');
+    table.dropColumn('output');
+    table.timestamp('created_at').notNullable().defaultTo(knex.fn.now());
+    table.timestamp('ended_at');
+  });
+
+};
+
+/**
+ * @param { import('knex').Knex } knex
+ * @returns { Promise<void> }
+ */
+export async function down(knex) {
+  await knex.schema.dropTable('MessageTracking');
+  await knex.schema.table('ActionTracking', function(table) {
+    table.string('output');
+    table.string('type').notNullable();
+    table.dropColumn('created_at');
+    table.dropColumn('ended_at');
+  });
+};
