7322a69e2328f82924eac6f0e40d6724a8423e5b
Jean-Philippe Kha <jean-philippe.kha@filigran.io>
2024-04-24 08:52:19 +0200

    [frontend] change menu popover side appearance

--- portal-front/src/components/menu/menu-admin.tsx
+++ portal-front/src/components/menu/menu-admin.tsx
@@ -37,7 +37,7 @@ const ClosedMenuAdmin = () => {
         })}>
         <Settings className="h-4 w-4" />
       </PopoverTrigger>
-      <PopoverContent>
+      <PopoverContent side="right">
         <AdminLinks />
       </PopoverContent>
     </Popover>
