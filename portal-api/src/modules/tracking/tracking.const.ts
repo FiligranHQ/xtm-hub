658d8de95943437253f8264afd975783a59257e7
Jean-Philippe Kha <jean-philippe.kha@filigran.io>
2024-04-25 11:53:55 +0200

    [backend] Rework message tracking and action tracking

---
+++ portal-api/src/modules/tracking/tracking.const.ts
@@ -0,0 +1,15 @@
+export const TrackingConst = {
+  'GET_AWX_WORKFLOW_REQUEST': {
+    type: 'GET_AWX_WORKFLOW_REQUEST',
+    technical: true,
+  },
+  'EXECUTE_AWX_REQUEST': {
+    type: 'EXECUTE_AWX_REQUEST',
+    technical: true,
+  },
+  'END_AWX_PROCESS': {
+    type: 'END_AWX_PROCESS',
+    technical: false,
+  },
+
+};
