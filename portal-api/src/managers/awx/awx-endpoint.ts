9f450540e4b68d5298c0b38254eb31f241e7d9d7
Jean-Philippe Kha <jean-philippe.kha@filigran.io>
2024-04-26 09:06:21 +0200

    [backend] Refactor AWX workflow tracking

--- portal-api/src/managers/awx/awx-endpoint.ts
+++ portal-api/src/managers/awx/awx-endpoint.ts
@@ -1,11 +1,11 @@
-import { endTracking } from '../../modules/tracking/tracking.domain';
+import { endAWXTracking } from '../../modules/tracking/tracking.domain';
 
 export const awxEndpoint = (app) => {
   app.post(`/awx/callback`, async (req, res, next) => {
     console.log('AWX req', req.body);
     if (req.body) {
       const { id: awxUUID, status, output } = req.body;
-      await endTracking(awxUUID, status, output);
+      await endAWXTracking(awxUUID, status, output);
 
     }
     res.send(200);
