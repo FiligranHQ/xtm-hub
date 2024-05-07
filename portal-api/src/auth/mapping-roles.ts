0c64db11289ffad2db9c8ca8e4a465596e25cd86
Jean-Philippe Kha <jean-philippe.kha@filigran.io>
2024-05-07 14:31:50 +0200

    [backend] Fix thales config

--- portal-api/src/auth/mapping-roles.ts
+++ portal-api/src/auth/mapping-roles.ts
@@ -14,5 +14,6 @@ const getRoleMapping = () => {
 
 const getUserRoles = (decodedUser) => {
   const rolesPath = config.get('user_management.roles_path') as string;
+  console.log(decodedUser['resource_access']['scred-portal-dev']);
   return getNestedPropertyValue(decodedUser, rolesPath);
 };
