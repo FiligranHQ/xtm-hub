a79f75ca04faf72704a68b6aec0e040304b09e60
thervy <tiffaine.hervy@filigran.io>
2024-06-06 16:12:12 +0200

    (frontend): (create-organisation): Add unit test

--- portal-front/src/components/organization/organization-create-sheet.test.tsx
+++ portal-front/src/components/organization/organization-create-sheet.test.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { OrganizationCreateSheet } from '@/components/organization/organization-create-sheet';
 import testRender from '@/utils/test/test-render';
+import { screen, fireEvent, waitFor } from '@testing-library/react';
 
 describe('OrganizationCreateSheet component', () => {
   it('render', async () => {
@@ -9,5 +10,32 @@ describe('OrganizationCreateSheet component', () => {
     );
 
     expect(!!element).toBe(true);
+  });
+
+  it('should enable create button on dirty form only', async () => {
+    testRender(<OrganizationCreateSheet setAddedOrganization={() => {}} />);
+    const openButton = screen.getByText('Organization');
+    fireEvent.click(openButton);
+
+    const createButton = screen.getByText('Create') as HTMLButtonElement;
+
+    // Initially, the button should be disabled
+    expect(createButton).toBeDisabled();
+
+    // Change the input to make the form dirty
+    const input = screen.getByPlaceholderText('Name');
+    fireEvent.change(input, { target: { value: 'New Organization' } });
+
+    // The button should now be enabled
+    await waitFor(() => {
+      expect(createButton).not.toBeDisabled();
+    });
+    // Clear the input to make the form dirty again
+    fireEvent.change(input, { target: { value: '' } });
+
+    // The button should be disabled again
+    await waitFor(() => {
+      expect(createButton).toBeDisabled();
+    });
   });
 });
