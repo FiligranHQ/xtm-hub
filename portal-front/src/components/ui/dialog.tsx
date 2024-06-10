d84ace4db829a267128cf557ee0f48277cbec5d7
thervy <tiffaine.hervy@filigran.io>
2024-06-10 16:02:01 +0200

    (frontend) (delete-orga): Deal with errors in UI

---
+++ portal-front/src/components/ui/dialog.tsx
@@ -0,0 +1,52 @@
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from 'filigran-ui/clients';
+import { FunctionComponent, ReactNode } from 'react';
+import { Button } from 'filigran-ui/servers';
+
+interface DialogInformativeProps {
+  isOpen: boolean;
+  onClose: () => void;
+  title: string;
+  description?: string;
+  children: ReactNode;
+}
+
+export const DialogInformative: FunctionComponent<DialogInformativeProps> = ({
+  isOpen,
+  onClose,
+  title,
+  description,
+  children,
+}) => {
+  return (
+    <Dialog
+      open={isOpen}
+      onOpenChange={onClose}>
+      <DialogContent className="max-w-xl">
+        <DialogHeader>
+          <DialogTitle>{title}</DialogTitle>
+          {description && <DialogDescription>{description}</DialogDescription>}
+        </DialogHeader>
+        {children}
+        <DialogFooter className="justify-end">
+          <DialogClose asChild>
+            <Button
+              className="mt-2"
+              type="button"
+              variant="secondary"
+              onClick={onClose}>
+              Close
+            </Button>
+          </DialogClose>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+};
