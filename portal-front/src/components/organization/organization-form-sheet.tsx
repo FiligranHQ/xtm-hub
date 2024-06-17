00ac941b6528649365737eaeda71c990c4566c1e
Jean-Philippe Kha <jean-philippe.kha@filigran.io>
2024-06-17 17:27:23 +0200

    [frontend/backend] refactor fetching organization

---
+++ portal-front/src/components/organization/organization-form-sheet.tsx
@@ -0,0 +1,102 @@
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from 'filigran-ui/clients';
+import { SheetDescription } from 'filigran-ui';
+import { Button, Input } from 'filigran-ui/servers';
+import * as React from 'react';
+import { FunctionComponent, ReactNode } from 'react';
+import { z } from 'zod';
+import { organizationFormSchema } from '@/components/organization/organization-form.schema';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { organizationItem_fragment$data } from '../../../__generated__/organizationItem_fragment.graphql';
+
+interface OrganizationFormSheetProps {
+  organization?: organizationItem_fragment$data;
+  open: boolean;
+  setOpen: (open: boolean) => void;
+  trigger: ReactNode;
+  title: string;
+  description: string;
+  handleSubmit: (values: any) => void;
+}
+export const OrganizationFormSheet: FunctionComponent<
+  OrganizationFormSheetProps
+> = ({
+  organization,
+  open,
+  setOpen,
+  trigger,
+  title,
+  description,
+  handleSubmit,
+}) => {
+  const form = useForm<z.infer<typeof organizationFormSchema>>({
+    resolver: zodResolver(organizationFormSchema),
+    defaultValues: {
+      name: organization?.name ?? '',
+    },
+  });
+  const onSubmit = (values: z.infer<typeof organizationFormSchema>) => {
+    handleSubmit({
+      ...values,
+    });
+  };
+  return (
+    <Sheet
+      key={'right'}
+      open={open}
+      onOpenChange={setOpen}>
+      <SheetTrigger asChild>{trigger}</SheetTrigger>
+      <SheetContent side={'right'}>
+        <SheetHeader>
+          <SheetTitle>{title}</SheetTitle>
+          <SheetDescription>{description}</SheetDescription>
+        </SheetHeader>
+        <Form {...form}>
+          <form
+            className="space-y-4 pt-8"
+            onSubmit={form.handleSubmit(onSubmit)}>
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="Name"
+                      {...field}
+                    />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+
+            <SheetFooter className="pt-2">
+              <SheetClose asChild>
+                <Button variant="outline">Cancel</Button>
+              </SheetClose>
+              <Button
+                disabled={!form.formState.isDirty}
+                type="submit">
+                Validate
+              </Button>
+            </SheetFooter>
+          </form>
+        </Form>
+      </SheetContent>
+    </Sheet>
+  );
+};
--- portal-front/src/components/organization/organization.service.ts
+++ portal-front/src/components/organization/organization.service.ts
@@ -1,11 +1,19 @@
 import { useLazyLoadQuery } from 'react-relay';
-import { organizationSelectQuery } from '../../../__generated__/organizationSelectQuery.graphql';
+import {
+  OrderingMode,
+  OrganizationOrdering,
+  organizationSelectQuery,
+} from '../../../__generated__/organizationSelectQuery.graphql';
 import { organizationFetch } from '@/components/organization/organization.graphql';
 
-export const getOrganizations = () => {
-  const queryOrganizationData = useLazyLoadQuery<organizationSelectQuery>(
-    organizationFetch,
-    {}
-  );
-  return queryOrganizationData.organizations.edges;
+export const getOrganizations = (
+  count: number = 10,
+  orderBy: OrganizationOrdering = 'name',
+  orderMode: OrderingMode = 'asc'
+) => {
+  return useLazyLoadQuery<organizationSelectQuery>(organizationFetch, {
+    count,
+    orderBy,
+    orderMode,
+  });
 };
