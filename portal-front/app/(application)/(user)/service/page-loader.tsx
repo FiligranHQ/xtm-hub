5ddde666c301fb3acb7bd0f19808be84522cc420
Jean-Philippe Kha <jean-philippe.kha@filigran.io>
2024-08-09 14:45:13 +0200

    [frontend] fix localStorage service-list

--- portal-front/app/(application)/(user)/service/page-loader.tsx
+++ portal-front/app/(application)/(user)/service/page-loader.tsx
@@ -6,29 +6,28 @@ import ServiceList from '@/components/service/service-list';
 import useMountingLoader from '@/hooks/useMountingLoader';
 import Loader from '@/components/loader';
 import { ServiceListQuery } from '@/components/service/service.graphql';
-import { serviceQuery } from '../../../../__generated__/serviceQuery.graphql';
+import {
+  OrderingMode,
+  ServiceOrdering,
+  serviceQuery,
+} from '../../../../__generated__/serviceQuery.graphql';
+import { useLocalStorage } from 'usehooks-ts';
 
 // Component interface
 interface ServiceListPreloaderProps {}
 
 // Component
 const PageLoader: React.FunctionComponent<ServiceListPreloaderProps> = () => {
-  let count = Number(localStorage.getItem('countServiceList'));
-  if (!count) {
-    localStorage.setItem('countServiceList', '50');
-    count = 50;
-  }
-  let orderMode = localStorage.getItem('orderModeServiceList');
-  if (!orderMode) {
-    localStorage.setItem('orderModeServiceList', 'asc');
-    orderMode = 'asc';
-  }
+  const [count, setCount] = useLocalStorage('countServiceList', 50);
+  const [orderMode, setOrderMode] = useLocalStorage<OrderingMode>(
+    'orderModeServiceList',
+    'asc'
+  );
+  const [orderBy, setOrderBy] = useLocalStorage<ServiceOrdering>(
+    'orderByServiceList',
+    'name'
+  );
 
-  let orderBy = localStorage.getItem('orderByServiceList');
-  if (!orderBy) {
-    localStorage.setItem('orderByServiceList', 'name');
-    orderBy = 'name';
-  }
   const [queryRef, loadQuery] = useQueryLoader<serviceQuery>(ServiceListQuery);
   useMountingLoader(loadQuery, { count, orderBy, orderMode });
   return <>{queryRef ? <ServiceList queryRef={queryRef} /> : <Loader />}</>;
