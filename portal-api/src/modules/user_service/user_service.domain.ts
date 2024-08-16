daa8591f43f5947739866a2f69bdeefa5fbcb3ce
Jean-Philippe Kha <jean-philippe.kha@filigran.io>
2024-08-16 12:26:50 +0200

    [backend] Create specific services table with left join service_link array

--- portal-api/src/modules/user_service/user_service.domain.ts
+++ portal-api/src/modules/user_service/user_service.domain.ts
@@ -7,6 +7,7 @@ import { UserId } from '../../model/kanel/public/User';
 import { SubscriptionId } from '../../model/kanel/public/Subscription';
 import { v4 as uuidv4 } from 'uuid';
 import { UserServiceConnection } from '../../__generated__/resolvers-types';
+import Service from '../../model/kanel/public/Service';
 
 export const insertUserService = async (
   context: PortalContext,
@@ -64,6 +65,21 @@ export const loadUserServiceByUser = async (
   userId,
   opts
 ) => {
+  const queryServicesWithLinks = db<Service>(context, 'Service')
+    .leftJoin(
+      'Service_Link as service_link',
+      'Service.id',
+      '=',
+      'service_link.service_id'
+    )
+    .select(
+      'Service.*',
+      dbRaw(
+        "( json_agg( json_build_object( 'name',service_link.name, 'url', service_link.url ) ) ):: json as services_link"
+      )
+    )
+    .groupBy(['Service.id', 'service_link.url']);
+
   const userServiceConnection = await paginate<UserService>(
     context,
     'User_Service',
@@ -76,18 +92,17 @@ export const loadUserServiceByUser = async (
       '=',
       'sub.id'
     )
-    .leftJoin('Service as service', 'sub.service_id', '=', 'service.id')
+    .leftJoin(
+      queryServicesWithLinks.as('service'),
+      'sub.service_id',
+      '=',
+      'service.id'
+    )
     .leftJoin(
       'Service_Capability as servcapa',
       'User_Service.id',
       '=',
       'servcapa.user_service_id'
-    )
-    .leftJoin(
-      'Service_Link as servlink',
-      'service.id',
-      '=',
-      'servlink.service_id'
     )
     .where('user.id', userId)
     .select([
@@ -96,7 +111,7 @@ export const loadUserServiceByUser = async (
         "(json_agg(json_build_object('id', \"user\".id, 'last_name', \"user\".last_name, 'first_name', \"user\".first_name, 'email', \"user\".email, '__typename', 'User')) ->> 0)::json as user"
       ),
       dbRaw(
-        `(json_agg(json_build_object('id', "sub".id, 'status', "sub".status, 'service_id', "sub".service_id, 'service', json_build_object('id', "service".id, 'name', "service".name, 'type', "service".type, 'provider', "service".provider, 'description', "service".description,  '__typename', 'Service'), '__typename', 'Subscription')) ->> 0)::json as subscription`
+        `(json_agg(json_build_object('id', "sub".id, 'status', "sub".status, 'service_id', "sub".service_id, 'service', json_build_object('id', "service".id, 'name', "service".name, 'type', "service".type, 'provider', "service".provider, 'description', "service".description, 'links', "service".services_link, '__typename', 'Service'), '__typename', 'Subscription')) ->> 0)::json as subscription`
       ),
       dbRaw(
         "(json_agg(json_build_object('id', \"servcapa\".id, 'service_capability_name', \"servcapa\".service_capability_name, '__typename', 'Service_Capability'))) as service_capability"
@@ -117,65 +132,8 @@ export const loadUserServiceByUser = async (
       'service.provider',
       'service.description',
       'sub.status',
-      'service_link.link',
     ])
     .asConnection<UserServiceConnection>();
-
-  // const userServiceConnection = await paginate<UserService>(
-  //   context,
-  //   'User_Service',
-  //   opts
-  // )
-  //   .leftJoin('User as user', 'User_Service.user_id', '=', 'user.id')
-  //   .leftJoin(
-  //     'Subscription as sub',
-  //     'User_Service.subscription_id',
-  //     '=',
-  //     'sub.id'
-  //   )
-  //   .leftJoin('Service as service', 'sub.service_id', '=', 'service.id')
-  //   .leftJoin(
-  //     'Service_Link as serviceLink',
-  //     'service.id',
-  //     '=',
-  //     'serviceLink.service_id'
-  //   )
-  //   .leftJoin(
-  //     'Service_Capability as servcapa',
-  //     'User_Service.id',
-  //     '=',
-  //     'servcapa.user_service_id'
-  //   )
-  //   .where('user.id', userId)
-  //   .select([
-  //     'User_Service.*',
-  //     dbRaw(
-  //       "(json_agg(json_build_object('id', \"user\".id,'last_name', \"user\".last_name, 'first_name', \"user\".first_name,  'email', \"user\".email, '__typename', 'User')) ->> 0)::json as user"
-  //     ),
-  //     dbRaw(
-  //       "(json_agg(json_build_object('id', \"sub\".id,'status', \"sub\".status,'service_id', \"sub\".service_id, 'service', json_build_object('id', \"service\".id,'name', \"service\".name,'type', \"service\".type,'provider', \"service\".provider,'description', \"service\".description,'link', json_agg(json_build_object('url', \"serviceLink\".url, '__typename', 'Service_Link')), '__typename', 'Service'), '__typename', 'Subscription')) ->> 0)::json as subscription"
-  //     ),
-  //     dbRaw(
-  //       "(json_agg(json_build_object('id', \"servcapa\".id, 'service_capability_name', \"servcapa\".service_capability_name, '__typename', 'Service_Capability'))) as service_capability"
-  //     ),
-  //     dbRaw('(service."name") as service_name'),
-  //     dbRaw('(service."provider") as service_provider'),
-  //     dbRaw('(service."type") as service_type'),
-  //     dbRaw('(service."description") as service_description'),
-  //     dbRaw('(sub."status") as subscription_status'),
-  //   ])
-  //   .groupBy([
-  //     'User_Service.id',
-  //     'user.first_name',
-  //     'user.last_name',
-  //     'user.email',
-  //     'service.name',
-  //     'service.type',
-  //     'service.provider',
-  //     'service.description',
-  //     'sub.status',
-  //   ])
-  //   .asConnection<UserServiceConnection>();
 
   userServiceConnection.edges = userServiceConnection.edges.map((edge) => {
     return {
