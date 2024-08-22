42ce20b3da58d830fabdb82c3ce4bd795d7e01ba
Jean-Philippe Kha <jean-philippe.kha@filigran.io>
2024-08-22 12:05:19 +0200

    [frontend] Add active link in menu and delete about page

--- portal-front/src/components/menu/menu-admin.tsx
+++ portal-front/src/components/menu/menu-admin.tsx
@@ -6,11 +6,10 @@ import {
   Popover,
   PopoverContent,
   PopoverTrigger,
-  Separator,
 } from 'filigran-ui/clients';
-import { Button } from 'filigran-ui/servers';
+import { Button, buttonVariants } from 'filigran-ui/servers';
 import * as React from 'react';
-import { ElementType, FunctionComponent, useCallback, useState } from 'react';
+import { ElementType, FunctionComponent, useEffect, useState } from 'react';
 import useGranted from '@/hooks/useGranted';
 import {
   ExternalReferenceIcon,
@@ -20,44 +19,130 @@ import {
   OrganizationIcon,
   SettingsIcon,
 } from 'filigran-icon';
-import { useRouter } from 'next/navigation';
+import { usePathname } from 'next/navigation';
 import { RESTRICTION } from '@/utils/constant';
+import { cn } from '@/lib/utils';
+import Link from 'next/link';
 
 export interface MenuAdminProps {
   open: boolean;
 }
+const OpenedMenuAdmin = () => (
+  <Accordion
+    type="single"
+    collapsible
+    className="w-full">
+    <AccordionItem
+      className="border-none"
+      value="item-1">
+      <AccordionTrigger className="h-9 justify-normal px-4 py-2">
+        <span className="flex w-8 flex-shrink-0 justify-center">
+          <SettingsIcon className="h-4 w-4" />
+        </span>
+        <span className="flex-1 px-2 text-left txt-default">Admin</span>
+      </AccordionTrigger>
+      <AccordionContent>
+        <AdminLinks className="h-8 txt-sub-content" />
+      </AccordionContent>
+    </AccordionItem>
+  </Accordion>
+);
+
+const ClosedMenuAdmin = () => {
+  const [adminOpened, setAdminOpened] = useState<boolean>(false);
+  const currentPath = usePathname();
+  useEffect(() => {
+    setAdminOpened(false);
+  }, [currentPath]);
+  return (
+    <Popover
+      open={adminOpened}
+      onOpenChange={setAdminOpened}>
+      <PopoverTrigger asChild>
+        <Button
+          variant="ghost"
+          className={cn(
+            'h-9 rounded-none',
+            currentPath.startsWith('/admin/') &&
+              'bg-primary/10 shadow-[inset_2px_0px] shadow-primary'
+          )}
+          aria-label="Settings menu">
+          <span className="flex w-8 flex-shrink-0 justify-center">
+            <SettingsIcon className="h-4 w-4" />
+          </span>
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent
+        sideOffset={0}
+        className="w-min border-border-light"
+        side="right"
+        align="start">
+        <AdminLinks className="pl-0" />
+      </PopoverContent>
+    </Popover>
+  );
+};
+const adminLinksData = [
+  { href: '/admin/user', icon: GroupIcon, label: 'User' },
+  { href: '/admin/service', icon: GradeIcon, label: 'Services' },
+  { href: '/admin/community', icon: ForumIcon, label: 'Communities' },
+  {
+    href: '/admin/organizations',
+    icon: OrganizationIcon,
+    label: 'Organizations',
+  },
+  {
+    href: '/admin/subcriptions',
+    icon: ExternalReferenceIcon,
+    label: 'Subscriptions',
+  },
+];
+
+const AdminLinks = ({ className }: { className?: string }) => (
+  <>
+    {adminLinksData.map(({ href, icon, label }) => (
+      <AdminButton
+        key={href}
+        className={className}
+        href={href}
+        icon={icon}
+        label={label}
+      />
+    ))}
+  </>
+);
 
 const AdminButton = ({
-  onClick,
+  href,
   icon: Icon,
   label,
   className,
 }: {
-  onClick: () => void;
+  href: string;
   icon: ElementType;
   label: string;
   className?: string;
-}) => (
-  <Button
-    onClick={onClick}
-    variant="ghost"
-    className={`h-9 w-full justify-start border-none ${className}`}>
-    <span className="ml-1 mr-2 flex w-8 justify-center">
-      <Icon className="h-4 w-4" />{' '}
-    </span>
-    {label}
-  </Button>
-);
-
+}) => {
+  const currentPath = usePathname();
+  return (
+    <Link
+      href={href}
+      className={cn(
+        buttonVariants({
+          variant: 'ghost',
+          className: `${className} h-9 w-full justify-start rounded-none border-none`,
+        }),
+        currentPath === href &&
+          'bg-primary/10 shadow-[inset_2px_0px] shadow-primary'
+      )}>
+      <span className="ml-1 mr-2 flex w-8 flex-shrink-0 justify-center">
+        <Icon className="h-4 w-4" />
+      </span>
+      {label}
+    </Link>
+  );
+};
 const MenuAdmin: FunctionComponent<MenuAdminProps> = ({ open }) => {
-  const [adminOpened, setAdminOpened] = useState<boolean>(false);
-  const router = useRouter();
-
-  const routeTo = useCallback((link: string) => {
-    router.replace(link);
-    setAdminOpened(false);
-  }, []);
-
   const useGrantedBYPASS = useGranted(RESTRICTION.CAPABILITY_BYPASS);
   const useGrantedFRT_MANAGE_USER = useGranted(
     RESTRICTION.CAPABILITY_FRT_MANAGE_USER
@@ -70,85 +155,6 @@ const MenuAdmin: FunctionComponent<MenuAdminProps> = ({ open }) => {
   );
   const useGrantedBCK_MANAGE_COMMUNITIES = useGranted(
     RESTRICTION.CAPABILITY_BCK_MANAGE_COMMUNITIES
-  );
-
-  const AdminLinks = ({ className }: { className?: string }) => (
-    <>
-      <AdminButton
-        className={className}
-        onClick={() => routeTo('/admin/user')}
-        icon={GroupIcon}
-        label="User"
-      />
-      <AdminButton
-        className={className}
-        onClick={() => routeTo('/admin/service')}
-        icon={GradeIcon}
-        label="Services"
-      />
-      <AdminButton
-        className={className}
-        onClick={() => routeTo('/admin/community')}
-        icon={ForumIcon}
-        label="Communities"
-      />
-      <AdminButton
-        className={className}
-        onClick={() => routeTo('/admin/organizations')}
-        icon={OrganizationIcon}
-        label="Organizations"
-      />
-      <AdminButton
-        className={className}
-        onClick={() => routeTo('/admin/subcriptions')}
-        icon={ExternalReferenceIcon}
-        label="Subscriptions"
-      />
-    </>
-  );
-
-  const OpenedMenuAdmin = () => (
-    <Accordion
-      type="single"
-      collapsible
-      className="w-full">
-      <AccordionItem
-        className="border-none"
-        value="item-1">
-        <AccordionTrigger className="h-9 justify-normal px-4 py-2">
-          <span className="flex w-8 flex-shrink-0 justify-center">
-            <SettingsIcon className="h-4 w-4" />
-          </span>
-          <span className="flex-1 px-2 text-left txt-default">Settings</span>
-        </AccordionTrigger>
-        <AccordionContent>
-          <AdminLinks className="h-8 txt-sub-content" />
-        </AccordionContent>
-      </AccordionItem>
-    </Accordion>
-  );
-
-  const ClosedMenuAdmin = () => (
-    <Popover
-      open={adminOpened}
-      onOpenChange={setAdminOpened}>
-      <PopoverTrigger asChild>
-        <Button
-          variant="ghost"
-          className="h-9"
-          aria-label="Settings menu">
-          <span className="flex w-8 flex-shrink-0 justify-center">
-            <SettingsIcon className="h-4 w-4" />
-          </span>
-        </Button>
-      </PopoverTrigger>
-      <PopoverContent
-        className="w-min"
-        side="right"
-        align="start">
-        <AdminLinks />
-      </PopoverContent>
-    </Popover>
   );
 
   if (
