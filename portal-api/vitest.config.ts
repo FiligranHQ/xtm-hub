1c29d2793f553923a776607be865675ee1df3ac4
Jean-Philippe Kha <jean-philippe.kha@filigran.io>
2024-09-03 15:41:54 +0200

    [backend] small modifications

--- portal-api/vitest.config.ts
+++ portal-api/vitest.config.ts
@@ -4,6 +4,14 @@ export default defineConfig({
   test: {
     globals: true,
     globalSetup: './tests/config-test.ts',
+    poolOptions: {
+      threads: {
+        singleThread: true,
+      },
+      fork: {
+        singleFork: true,
+      },
+    },
   },
   plugins: [], // only if you are using custom tsconfig paths
 });
