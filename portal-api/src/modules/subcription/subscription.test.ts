a33913002d30de0dc5e2b25993505c6f83832bfa
thervy <tiffaine.hervy@filigran.io>
2024-09-10 15:11:18 +0200

    [backend] - fix tests

--- portal-api/src/modules/subcription/subscription.test.ts
+++ portal-api/src/modules/subcription/subscription.test.ts
@@ -7,12 +7,10 @@ import { fromGlobalId, toGlobalId } from 'graphql-relay/node/node.js';
 import { dbUnsecure } from '../../../knexfile';
 import UserService from '../../model/kanel/public/UserService';
 import { ServiceId } from '../../model/kanel/public/Service';
-import {
-  deleteSubscriptionUnsecure,
-  loadUnsecureSubscriptionBy,
-} from './subscription.helper';
+import { loadUnsecureSubscriptionBy } from './subscription.helper';
+import Subscription from '../../model/kanel/public/Subscription';
 
-const addSubscriptionTestQuery = (serviceId) => ({
+const addSubscriptionTestQuery = {
   query: print(gql`
     mutation addSubscriptionMutation($service_id: String) {
       addSubscription(service_id: $service_id) {
@@ -21,9 +19,9 @@ const addSubscriptionTestQuery = (serviceId) => ({
     }
   `),
   variables: {
-    service_id: toGlobalId('Service', serviceId),
+    service_id: toGlobalId('Service', 'c6343882-f609-4a3f-abe0-a34f8cb11302'),
   },
-});
+};
 const getSubscriptionsByServiceIdTestQuery = {
   query: print(gql`
     query subscriptionsByServiceIdQuery($service_id: ID) {
@@ -53,52 +51,57 @@ const getSubscriptionsByServiceIdTestQuery = {
 describe('UserAdmin can subscribe to a service', async () => {
   const userAdmin = await getAdminAgent();
 
-  it.each`
-    serviceId                                 | subscriptionType          | numberOfUserServiceAccess
-    ${'c6343882-f609-4a3f-abe0-a34f8cb11302'} | ${'Subscriptable_direct'} | ${'2'}
-  `(
-    'Should add service $subscriptionStatus for service $subscriptionTyp e',
-    async ({ serviceId, numberOfUserServiceAccess }) => {
-      const response = await userAdmin
-        .post('/graphql-api')
-        .send(addSubscriptionTestQuery(serviceId));
-      const transform = JSON.parse(response.text);
-      expect(response.status).toBe(200);
+  it('Should add subscription for service SUBSCRIPTABLE_DIRECT', async () => {
+    const response = await userAdmin
+      .post('/graphql-api')
+      .send(addSubscriptionTestQuery);
+    const transform = JSON.parse(response.text);
+    expect(response.status).toBe(200);
+    const [subscription] = await loadUnsecureSubscriptionBy({
+      service_id: fromGlobalId(transform.data.addSubscription.id)
+        .id as ServiceId,
+    });
+    // It should also add access for users in the organization
+    const userService = await dbUnsecure<UserService>('User_Service')
+      .where('User_Service.subscription_id', '=', subscription.id)
+      .countDistinct('id as totalCount')
+      .first();
 
-      const [subscription] = await loadUnsecureSubscriptionBy({
-        service_id: fromGlobalId(transform.data.addSubscription.id)
-          .id as ServiceId,
-      });
-      // It should also add access for users in the organization
-      const userService = await dbUnsecure<UserService>('User_Service')
-        .where('User_Service.subscription_id', '=', subscription.id)
+    expect(userService.totalCount).toEqual('2');
 
-        .countDistinct('id as totalCount')
-        .first();
-
-      expect(userService.totalCount).toEqual(numberOfUserServiceAccess);
-      // Clean everything
-      deleteSubscriptionUnsecure(
+    // Clean everything
+    await dbUnsecure<Subscription>('Subscription')
+      .where(
+        'Subscription.service_id',
+        '=',
         fromGlobalId(transform.data.addSubscription.id).id
-      );
-    }
-  );
+      )
+      .delete('*')
+      .returning('*');
+  });
 });
 
-describe('Should return Subscriptions by serviceId', async () => {
+describe('Should return Subscriptions by serviceId ', async () => {
   const userAdmin = await getAdminAgent();
   const response = await userAdmin
     .post('/graphql-api')
     .send(getSubscriptionsByServiceIdTestQuery);
   const transform = JSON.parse(response.text);
+
   it('Should return all subscriptions ', async () => {
     expect(response.status).toBe(200);
-    expect(transform.data.subscriptionsByServiceId.length).toEqual(2);
+    const [request] =
+      await dbUnsecure<Subscription>('Subscription').countDistinct(
+        'id as count'
+      );
+    expect(transform.data.subscriptionsByServiceId.length).toEqual(
+      parseInt(request.count)
+    );
   });
-  it('Should return correct user_service object', async () => {
+  it('Should return correct user_service object ', async () => {
     const userServiceTest = transform.data.subscriptionsByServiceId.find(
       ({ user_service }) => {
-        return user_service.length > 0;
+        return user_service?.length > 0;
       }
     );
     expect(userServiceTest.id).not.toBeNull();
