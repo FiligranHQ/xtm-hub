95a4646dd74692af9db812c48ba9afd2b33a0ca2
Jean-Philippe Kha <jean-philippe.kha@filigran.io>
2024-09-20 11:34:55 +0200

    [backend] Add type column in ActionTracking

---
+++ portal-api/src/migrations/20240920090042_add_awx_type_tracking.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export async function up(knex) {
+  await knex.schema.table('ActionTracking', function (table) {
+    table.string('type').notNullable();
+  });
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export async function down(knex) {
+  await knex.schema.table('ActionTracking', function (table) {
+    table.dropColumn('type');
+  });
+}
