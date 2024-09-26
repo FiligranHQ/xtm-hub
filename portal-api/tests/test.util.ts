302ecade6f667bd3747205144fbca030206988c3
Jean-Philippe Kha <jean-philippe.kha@filigran.io>
2024-09-26 12:39:15 +0200

    [config] test

--- portal-api/tests/test.util.ts
+++ portal-api/tests/test.util.ts
@@ -2,7 +2,7 @@ import request from 'supertest';
 import { print } from 'graphql/language';
 import gql from 'graphql-tag';
 
-const ENDPOINT_URL = 'http://portal-api:4001';
+const ENDPOINT_URL = 'http://127.0.0.1:4001';
 
 export const logAdminUserTest = async (agent) => {
   return await agent.post('/graphql-api').send({
