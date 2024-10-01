21bb90ebd58aa59f2288326321d5877bf3ebc5ff
Jean-Philippe Kha <jean-philippe.kha@filigran.io>
2024-10-01 13:36:07 +0200

    [backend] refactor test

--- portal-api/src/modules/users/users.domain.test.ts
+++ portal-api/src/modules/users/users.domain.test.ts
@@ -1,11 +1,12 @@
 import { describe, expect, it } from 'vitest';
 import { loadUserBy } from './users.domain';
 import { UserId } from '../../model/kanel/public/User';
+import { ADMIN_UUID } from '../../portal.const';
 
 describe('Users domain', () => {
   it('should be logged user', async () => {
     const response = await loadUserBy({
-      'User.id': 'e389e507-f1cd-4f2f-bfb2-274140d87d28' as UserId,
+      'User.id': ADMIN_UUID as UserId,
     });
     expect(response).toBeTruthy();
   });
