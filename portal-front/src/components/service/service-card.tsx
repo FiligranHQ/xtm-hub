5bfa4572e4dd7024ef3dd167543a2d244600e66c
thervy <tiffaine.hervy@filigran.io>
2024-10-03 15:24:24 +0200

    [frontend] - Small UI changes

--- portal-front/src/components/service/service-card.tsx
+++ portal-front/src/components/service/service-card.tsx
@@ -6,34 +6,40 @@ import {serviceList_fragment$data} from "../../../__generated__/serviceList_frag
 
 interface ServiceCardProps {
     service: serviceList_fragment$data;
-    action: ReactNode;
+    topRightAction: ReactNode;
+    bottomLeftAction: ReactNode;
 }
 const ServiceCard: React.FunctionComponent<ServiceCardProps> = ({
     service,
-    action
+    topRightAction,
+    bottomLeftAction
    }) => {
     const t = useTranslations();
 
+    console.log("bottomLeftAction", bottomLeftAction)
     return (<li
         className="border-light flex flex-col rounded border bg-page-background p-s"
         key={service.id}>
         <div className="flex-1 p-m pb-xl flex justify-between items-center gap-s">
             <h3>{service.name}</h3>{' '}
 
-            {action}
+            {topRightAction}
 
         </div>
         <p className={'p-m pb-xl pt-s txt-sub-content'}>
             {service.description}
         </p>
-        <ul className="flex justify-between items-center p-s gap-s flex-row">
-            <li>
+        <div className="flex justify-between items-center p-s gap-s flex-row">
+            <div>
                 <ServiceTypeBadge
                     type={service.type as ServiceTypeBadge}
                 />
-            </li>
+            </div>
+            <div>
+            {bottomLeftAction}
+            </div>
 
-        </ul>
+        </div>
     </li>)
 }
 export default ServiceCard;
