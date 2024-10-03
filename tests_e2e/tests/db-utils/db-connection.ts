2416e197cddbb62e1d824d7823d74e73616d15fc
thervy <tiffaine.hervy@filigran.io>
2024-10-03 14:06:02 +0200

    [e2e] - DB connection with Knex

--- tests_e2e/tests/db-utils/db-connection.ts
+++ tests_e2e/tests/db-utils/db-connection.ts
@@ -1,11 +1,12 @@
-import { Pool } from 'pg';
+import knex from 'knex';
 
-const pool = new Pool({
-    user: process.env.E2E_BASE_URL ? process.env.POSTGRES_USER: "portal",
-    host: process.env.E2E_BASE_URL ? process.env.DATABASE_HOST:"127.0.0.1",
-    database:process.env.E2E_BASE_URL ? process.env.POSTGRES_DB:"cloud-portal",
-    password: process.env.E2E_BASE_URL ? process.env.POSTGRES_PASSWORD:"portal-password",
-    port:process.env.E2E_BASE_URL ? process.env.DATABASE_PORT: 5432,
+export const db = knex({
+    client: 'pg',
+    connection: {
+        user: process.env.E2E_BASE_URL ? process.env.POSTGRES_USER : "portal",
+        host: process.env.E2E_BASE_URL ? process.env.DATABASE_HOST : "127.0.0.1",
+        database: process.env.E2E_BASE_URL ? process.env.POSTGRES_DB : 'cloud-portal',
+        password: process.env.E2E_BASE_URL ? process.env.POSTGRES_PASSWORD : 'portal-password',
+        port: process.env.E2E_BASE_URL ? process.env.DATABASE_PORT : 5432,
+    },
 });
-
-export const query = (text: string, params?: any[]) => pool.query(text, params);
