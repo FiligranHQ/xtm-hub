8e19b8830bde3cc3fe09227becd459c1b94e4827
Jean-Philippe Kha <jean-philippe.kha@filigran.io>
2024-10-25 15:07:36 +0200

    [backend] Customize email template

--- portal-api/src/server/mail-service.ts
+++ portal-api/src/server/mail-service.ts
@@ -1,20 +1,50 @@
 import config from 'config';
+import * as fsPromises from 'fs/promises';
 import nodemailer from 'nodemailer';
+import * as path from 'path';
+
 const smtpOptions = config.get('smtp_options');
 const transporter = nodemailer.createTransport(smtpOptions);
 
+async function renderEmail<T extends Record<string, string>>(substitutions: T) {
+  const templatePath = 'transactional';
+  const filepath = path.join(
+    'src/server/mail-template',
+    `${templatePath}.html`
+  );
+  let template = (await fsPromises.readFile(filepath)).toString();
+
+  for (const sub in substitutions) {
+    template = template.replace(`{{ ${sub} }}`, substitutions[sub]);
+  }
+
+  return template;
+}
+
 export const sendMail = async ({
   from = (config.get('smtp_options.auth.user') as string) ||
-    'no-reply@scredplatform.io',
-  to = 'berof12139@foraro.com',
+    'openbas-dev@filigran.cloud',
+  to = 'openbas-dev@filigran.cloud',
   subject = 'test mail',
-  text = 'Hello This is an SMTP message with customizations',
+  text = 'Please confirm your email address in order to activate your account.',
 }) => {
+  const html = await renderEmail({
+    name: 'Jean-Philippe',
+    project: 'XTM Hub',
+    text,
+  });
   const mailOptions = {
     from,
     to,
     subject,
-    text,
+    html,
+    attachments: [
+      {
+        filename: 'logo_filigran.png',
+        path: 'src/server/mail-template/images/logo_filigran.png',
+        cid: 'logo_filigran.png',
+      },
+    ],
   };
   transporter.sendMail(mailOptions, (error, info) => {
     if (error) {
