00a42ba5d4d9dabc24eec3bd4d2e365ff418e64d
jpkha <jean-philippe.kha@filigran.io>
2024-11-04 14:02:04 +0100

    [backend] Fix migration and add eslint and ts verification in test:ci (#39)
    * [backend] Fix migration and add eslint and ts verification in test:ci
    
    * [config] Cancel previous job automatically

--- portal-api/src/modules/services/file/file-storage.ts
+++ portal-api/src/modules/services/file/file-storage.ts
@@ -1,55 +1,64 @@
-import * as s3 from '@aws-sdk/client-s3';
 import { Upload as S3Upload } from '@aws-sdk/lib-storage';
 import config from 'config';
-import {Readable} from "stream";
-import { S3Client, CreateBucketCommand, HeadBucketCommand } from "@aws-sdk/client-s3";
+import { Readable } from 'stream';
+import {
+  S3Client,
+  CreateBucketCommand,
+  HeadBucketCommand,
+} from '@aws-sdk/client-s3';
 
 const getEndpoint = () => {
-    // If using AWS S3, unset the endpoint to let the library choose the best endpoint
-    if (config.get('minio.endpoint') === 's3.amazonaws.com') {
-        return undefined;
-    }
-    return `${(config.get('minio.useSsl') === 'true' ? 'https' : 'http')}://${config.get('minio.endpoint')}:${config.get('minio.port')}`;
+  // If using AWS S3, unset the endpoint to let the library choose the best endpoint
+  if (config.get('minio.endpoint') === 's3.amazonaws.com') {
+    return undefined;
+  }
+  return `${config.get('minio.useSsl') === 'true' ? 'https' : 'http'}://${config.get('minio.endpoint')}:${config.get('minio.port')}`;
 };
 
 const s3Client = new S3Client({
-    region: config.get('minio.region'),
-    endpoint: getEndpoint(),
-    forcePathStyle: true,
-    credentials: {
-        accessKeyId: config.get('minio.accessKeyId'),
-        secretAccessKey: config.get('minio.secretAccessKey'),
-    },
-    tls: config.get('minio.useSsl') === 'true' ? true : false
+  region: config.get('minio.region'),
+  endpoint: getEndpoint(),
+  forcePathStyle: true,
+  credentials: {
+    accessKeyId: config.get('minio.accessKeyId'),
+    secretAccessKey: config.get('minio.secretAccessKey'),
+  },
+  tls: config.get('minio.useSsl') === 'true' ? true : false,
 });
 
 export interface UploadedFile {
-        createReadStream: () => Readable;
-        filename: string;
-        mimetype: string;
-        encoding: string;
+  createReadStream: () => Readable;
+  filename: string;
+  mimetype: string;
+  encoding: string;
 }
 export const isStorageAlive = () => initializeBucket();
 export const initializeBucket = async () => {
-    try {
-        // Try to access to the bucket
-        await s3Client.send(new HeadBucketCommand({ Bucket: config.get('minio.bucketName') }));
-        return true;
-    } catch (err) {
-        // If bucket not exist, try to create it.
-        // If creation fail, propagate the exception
-        await s3Client.send(new CreateBucketCommand({ Bucket: config.get('minio.bucketName') }));
-        return true;
-    }
+  try {
+    // Try to access to the bucket
+    await s3Client.send(
+      new HeadBucketCommand({ Bucket: config.get('minio.bucketName') })
+    );
+    return true;
+  } catch (err) {
+    // If bucket not exist, try to create it.
+    // If creation fail, propagate the exception
+
+    console.error(err);
+    await s3Client.send(
+      new CreateBucketCommand({ Bucket: config.get('minio.bucketName') })
+    );
+    return true;
+  }
 };
 export const insertFileInMinio = async (fileParams) => {
-    const fileKey = fileParams.Key;
-    const s3Upload = new S3Upload({
-        client: s3Client,
-        params: fileParams
-    });
-    await s3Upload.done();
-    console.log("inserted file ", fileParams.Key + ' into Minio...')
+  const fileKey = fileParams.Key;
+  const s3Upload = new S3Upload({
+    client: s3Client,
+    params: fileParams,
+  });
+  await s3Upload.done();
+  console.log('inserted file ', fileParams.Key + ' into Minio...');
 
-    return await fileKey
-}
+  return await fileKey;
+};
