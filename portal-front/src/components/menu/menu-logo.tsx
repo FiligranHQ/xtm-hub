6b9453007ee6697442cd442038c43d58624d0f7e
Jean-Philippe Kha <jean-philippe.kha@filigran.io>
2024-11-19 10:15:14 +0100

    [frontend] Fix scroll with sticky header

--- portal-front/src/components/menu/menu-logo.tsx
+++ portal-front/src/components/menu/menu-logo.tsx
@@ -2,7 +2,7 @@ import { DisplayLogo } from '@/components/ui/display-logo';
 
 export const MenuLogo = () => {
   return (
-    <div className="flex flex-shrink-0 sticky top-0 h-16 border-border-light border-b bg-page-background items-center px-l">
+    <div className="flex z-10 flex-shrink-0 sticky top-0 h-16 border-border-light border-b bg-page-background items-center px-l">
       <DisplayLogo className="w-[10rem] absolute" />
     </div>
   );
