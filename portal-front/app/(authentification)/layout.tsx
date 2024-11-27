4c3ee0224e938d0535dd84685a58e4a9532eac66
Jean-Philippe Kha <jean-philippe.kha@filigran.io>
2024-11-27 10:25:06 +0100

    [frontend] Add health route

--- portal-front/app/(authentification)/layout.tsx
+++ portal-front/app/(authentification)/layout.tsx
@@ -1,11 +1,16 @@
 import AppContext from '@/components/app-context';
 import I18nContext from '@/i18n/i18n-context';
+import { Metadata } from 'next';
+import { headers } from 'next/headers';
 import * as React from 'react';
 
-export const metadata = {
-  title: 'XTM Hub',
-  description: 'XTM Hub application by Filigran',
-};
+export async function generateMetadata(): Promise<Metadata> {
+  return {
+    title: 'XTM Hub',
+    description: 'XTM Hub application by Filigran',
+    metadataBase: new URL(`https://${headers().get('host')}`),
+  };
+}
 
 export default function RootLayout({
   children,
