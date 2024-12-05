e96d1bb8d3d8d1f1748b8d983cd3655c17520eed
Jean-Philippe Kha <jean-philippe.kha@filigran.io>
2024-12-05 16:25:52 +0100

    [frontend/backend] feat: remove user from an organization

---
+++ portal-front/src/components/admin/user/remove-user-from-orga.tsx
@@ -0,0 +1,80 @@
+import { AlertDialogComponent } from '@/components/ui/alert-dialog';
+import {
+  IconActionContext,
+  IconActionsButton,
+} from '@/components/ui/icon-actions';
+import { useToast } from 'filigran-ui/clients';
+import { useTranslations } from 'next-intl';
+import { FunctionComponent, ReactNode, useContext } from 'react';
+import { graphql, useMutation } from 'react-relay';
+import { removeUserFromOrgaMutation } from '../../../../__generated__/removeUserFromOrgaMutation.graphql';
+
+interface RemoveUserFromOrgaProps {
+  user: {
+    id: string;
+    email: string;
+  };
+  organization_id: string;
+  trigger?: ReactNode;
+  connectionID: string;
+}
+
+const removeUser = graphql`
+  mutation removeUserFromOrgaMutation(
+    $connections: [ID!]!
+    $user_id: ID!
+    $organization_id: ID!
+  ) {
+    removeUserFromOrganization(
+      user_id: $user_id
+      organization_id: $organization_id
+    ) {
+      id @deleteEdge(connections: $connections)
+    }
+  }
+`;
+
+export const RemoveUserFromOrga: FunctionComponent<RemoveUserFromOrgaProps> = ({
+  user,
+  organization_id,
+  trigger,
+  connectionID,
+}) => {
+  const { setMenuOpen } = useContext(IconActionContext);
+  const { toast } = useToast();
+  const t = useTranslations();
+  const [removeUserMutation] =
+    useMutation<removeUserFromOrgaMutation>(removeUser);
+  const onRemoveUser = (user_id: string): void => {
+    setMenuOpen(false);
+    removeUserMutation({
+      variables: { user_id, organization_id, connections: [connectionID] },
+      onError: (error) => {
+        toast({
+          variant: 'destructive',
+          title: t('Utils.Error'),
+          description: <>{error.message}</>,
+        });
+      },
+    });
+  };
+
+  const defaultTrigger = (
+    <IconActionsButton aria-label={t('UserActions.RemoveUser')}>
+      {t('MenuActions.Remove')}
+    </IconActionsButton>
+  );
+
+  return (
+    <AlertDialogComponent
+      AlertTitle={t('UserActions.RemoveUser')}
+      actionButtonText={t('MenuActions.Remove')}
+      variantName={'destructive'}
+      triggerElement={trigger ?? defaultTrigger}
+      onClickContinue={() => onRemoveUser(user.id)}>
+      {t('RemoveUserOrgDialog.TextRemoveThisUser', {
+        email: user.email,
+      })}
+    </AlertDialogComponent>
+  );
+};
