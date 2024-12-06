081cc77a3fc6c75a66c756896a40fefb31c65433
Jean-Baptiste Alleaume <jean-baptiste.alleaume@filigran.io>
2024-12-06 17:54:33 +0100

    feat: frontend log forward

--- portal-front/app/(application)/(admin)/error.tsx
+++ portal-front/app/(application)/(admin)/error.tsx
@@ -1,12 +1,29 @@
 'use client';
 
+import { logFrontendError } from '@/components/error-frontend-log.graphql';
+import { useEffect } from 'react';
+import { useRelayEnvironment } from 'react-relay';
+
 export default function Error({
   error,
   reset,
 }: {
-  error: Error & { digest?: string };
+  error: Error & { digest?: string; componentStack?: string };
   reset: () => void;
 }) {
+  const environment = useRelayEnvironment();
+
+  useEffect(() => {
+    if (error) {
+      logFrontendError(
+        environment,
+        error.message || 'Unknown error',
+        error.stack,
+        error.componentStack
+      );
+    }
+  }, [error, environment]);
+
   return (
     <div>
       <h2>Something went wrong!</h2>
