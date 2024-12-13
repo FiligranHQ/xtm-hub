ffc8b71ae4da07e48428833ada9c4efa7b0bdcd7
Jean-Philippe Kha <jean-philippe.kha@filigran.io>
2024-12-13 17:14:23 +0100

    [frontend] fix:Change title and add domain validation in Organization create/edit form #171

--- portal-front/src/components/organization/organization-form.schema.ts
+++ portal-front/src/components/organization/organization-form.schema.ts
@@ -1,9 +1,11 @@
 import { z } from 'zod';
 
 export const organizationFormSchema = z.object({
-  name: z.string().min(2, { message: 'Name must be at least 2 characters.' }),
+  name: z.string().min(2, { message: 'OrganizationForm.Error.Name' }),
   domains: z
     .string()
     .array()
-    .refine((domains) => domains.length > 0),
+    .refine((domains) => domains.length > 0, {
+      message: 'OrganizationForm.Error.Domain',
+    }),
 });
