caf1eceb8db68f9e723a9663ffdc96e1bac2421a
Jean-Baptiste Alleaume <jean-baptiste.alleaume@filigran.io>
2025-01-07 11:33:11 +0100

    styles: fixes

--- portal-front/src/components/admin/admin-callout.tsx
+++ portal-front/src/components/admin/admin-callout.tsx
@@ -11,19 +11,19 @@ export function AdminCallout() {
 
   useEffect(() => {
     if (isAdminPath) {
-      document.body.classList.add('pt-9');
+      document.body.classList.add('!pt-9');
     } else {
-      document.body.classList.remove('pt-9');
+      document.body.classList.remove('!pt-9');
     }
 
-    return () => document.body.classList.remove('pt-9');
+    return () => document.body.classList.remove('!pt-9');
   }, [isAdminPath]);
 
   return (
     isAdminPath && (
       <Callout
         variant="warning"
-        className="absolute z-50 top-0 left-0 right-0 rounded-none">
+        className="absolute z-50 top-0 left-0 right-0 rounded-none !text-black">
         {t('AdminCallout')}
       </Callout>
     )
