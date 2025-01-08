94a3cea90ea9afe45f4a2a64c85de3e7478da8ca
Jean-Baptiste Alleaume <jean-baptiste.alleaume@filigran.io>
2025-01-08 14:35:44 +0100

    feat: parameters view

--- portal-front/src/components/admin/parameters/parameters.tsx
+++ portal-front/src/components/admin/parameters/parameters.tsx
@@ -1,3 +1,37 @@
+import {
+  Badge,
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+  Label,
+} from 'filigran-ui';
+import { useTranslations } from 'next-intl';
+
 export const Parameters = () => {
-  return <div>parameters</div>;
+  const t = useTranslations();
+  const appVersion = process.env.NEXT_PUBLIC_APP_VERSION || '0.0.0-dev';
+
+  return (
+    <>
+      <h1 className="pb-s"></h1>
+      <div className="grid grid-cols-4">
+        <Card className="w-1-3">
+          <CardHeader>
+            <CardTitle>XTM Hub</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <div className="flex flex-col gap-2">
+              <div className="flex items-center justify-between border-b py-2">
+                <Label>{t('Parameters.Version')}</Label>
+                <div>
+                  <Badge>{appVersion}</Badge>
+                </div>
+              </div>
+            </div>
+          </CardContent>
+        </Card>
+      </div>
+    </>
+  );
 };
