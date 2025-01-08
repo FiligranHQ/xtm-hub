cac5ea224491b5c5f7a4cd894498bfff89bd766a
Jean-Baptiste Alleaume <jean-baptiste.alleaume@filigran.io>
2025-01-08 14:40:08 +0100

    fix: bad markup

--- portal-front/src/components/admin/parameters/parameters.tsx
+++ portal-front/src/components/admin/parameters/parameters.tsx
@@ -13,25 +13,22 @@ export const Parameters = () => {
   const appVersion = process.env.NEXT_PUBLIC_APP_VERSION || '0.0.0-dev';
 
   return (
-    <>
-      <h1 className="pb-s"></h1>
-      <div className="grid grid-cols-3">
-        <Card className="w-1-3">
-          <CardHeader>
-            <CardTitle>XTM Hub</CardTitle>
-          </CardHeader>
-          <CardContent>
-            <div className="flex flex-col gap-2">
-              <div className="flex items-center justify-between border-b py-2">
-                <Label>{t('Parameters.Version')}</Label>
-                <div>
-                  <Badge>{appVersion}</Badge>
-                </div>
+    <div className="grid grid-cols-3">
+      <Card className="w-1-3">
+        <CardHeader>
+          <CardTitle>XTM Hub</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <div className="flex flex-col gap-2">
+            <div className="flex items-center justify-between border-b py-2">
+              <Label>{t('Parameters.Version')}</Label>
+              <div>
+                <Badge>{appVersion}</Badge>
               </div>
             </div>
-          </CardContent>
-        </Card>
-      </div>
-    </>
+          </div>
+        </CardContent>
+      </Card>
+    </div>
   );
 };
