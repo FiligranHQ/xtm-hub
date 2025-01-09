8622e5a75648ab8dcdfc4951a8d7b71a61e9d9a2
Jean-Baptiste Alleaume <jean-baptiste.alleaume@filigran.io>
2025-01-09 14:13:54 +0100

    ok

--- portal-api/src/auth/auth-user.ts
+++ portal-api/src/auth/auth-user.ts
@@ -10,7 +10,7 @@ import {
   ensureUserOrganizationExist,
   ensureUserRoleExist,
 } from '../server/initialize.helper';
-import { ForbiddenAccessError } from '../utils/error.util';
+import { ForbiddenAccess } from '../utils/error.util';
 import { isEmptyField } from '../utils/utils';
 
 export const loginFromProvider = async (userInfo: UserInfo) => {
@@ -18,7 +18,7 @@ export const loginFromProvider = async (userInfo: UserInfo) => {
   // endregion
   const { email } = userInfo;
   if (isEmptyField(email)) {
-    throw ForbiddenAccessError('User email not provided');
+    throw ForbiddenAccess('User email not provided');
   }
 
   const user = await getOrCreateUser(userInfo);
--- portal-api/src/utils/error.util.ts
+++ portal-api/src/utils/error.util.ts
@@ -1,43 +1,102 @@
 import { createError } from 'apollo-errors';
+import { logApp } from './app-logger.util';
 
 enum ErrorCategory {
-  BAD_REQUEST = 'BAD_REQUEST',
-  CONFLICT = 'CONFLICT',
-  TECHNICAL = 'TECHNICAL',
+  BadRequest = 'BAD_REQUEST',
+  Conflict = 'CONFLICT',
+  Technical = 'TECHNICAL',
 }
 
-export const errorUtil = (
-  name: string,
-  message?: string,
+export enum ErrorType {
+  ForbiddenAccess = 'FORBIDDEN_ACCESS',
+  UnknownError = 'UNKNOWN_ERROR',
+  StillReference = 'STILL_REFERENCED',
+  AlreadyExists = 'ALREADY_EXISTS',
+  NotFound = 'NOT_FOUND',
+}
+
+const errorUtil = (
+  name: ErrorType,
+  message: string,
+  data: Record<string, unknown> & {
+    genre?: ErrorCategory;
+    http_status?: number;
+  }
+) => {
+  const Exception = createError(name, { data, message });
+  return new Exception();
+};
+
+export const ForbiddenAccess = (
+  message: string,
   data?: Record<string, unknown>
 ) => {
-  const Exception = createError(name, { data, message: message ?? name });
-  return new Exception();
+  return errorUtil(
+    ErrorType.ForbiddenAccess,
+    message || 'You are not allowed to do this.',
+    {
+      http_status: 403,
+      genre: ErrorCategory.Technical,
+      ...data,
+    }
+  );
+};
+
+export const UnknownError = (
+  message: string,
+  details?: Record<string, unknown>,
+  data?: Record<string, unknown>
+) => {
+  logApp.error(message + ' details: ' + details);
+  return errorUtil(
+    ErrorType.UnknownError,
+    message || 'An unknown error has occurred',
+    {
+      http_status: 500,
+      genre: ErrorCategory.Technical,
+      ...data,
+    }
+  );
+};
+
+export const StillReferencedError = (
+  message?: string,
+  details?: Record<string, unknown>,
+  data?: Record<string, unknown>
+) => {
+  logApp.error(message + ' details: ' + details);
+
+  return errorUtil(ErrorType.StillReference, message, {
+    http_status: 200,
+    genre: ErrorCategory.Conflict,
+    ...data,
+  });
 };
 
 export const AlreadyExistsError = (
   message?: string,
+  details?: Record<string, unknown>,
   data?: Record<string, unknown>
-) =>
-  errorUtil('ALREAY_EXISTS', message, {
-    http_status: 409,
-    genre: ErrorCategory.CONFLICT,
+) => {
+  logApp.error(message + ' details: ' + details);
+
+  return errorUtil(ErrorType.AlreadyExists, message, {
+    http_status: 200,
+    genre: ErrorCategory.Conflict,
     ...data,
   });
+};
 
-export const ForbiddenAccessError = (
+export const NotFoundError = (
   message?: string,
+  details?: Record<string, unknown>,
   data?: Record<string, unknown>
-) =>
-  errorUtil('FORBIDDEN_ACCESS', message, {
-    http_status: 403,
-    genre: ErrorCategory.TECHNICAL,
-    ...data,
-  });
+) => {
+  logApp.error(message + ' details: ' + details);
 
-export const UnknownError = (message: string, data?: Record<string, unknown>) =>
-  errorUtil('UNKNOWN_ERROR', message, {
-    http_status: 500,
-    genre: ErrorCategory.TECHNICAL,
+  return errorUtil(ErrorType.NotFound, message, {
+    http_status: 200,
+    genre: ErrorCategory.BadRequest,
     ...data,
   });
+};
