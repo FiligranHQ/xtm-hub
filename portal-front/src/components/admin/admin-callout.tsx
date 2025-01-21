9c61db1786c7b5d55226d053b310e72a375fa692
Jean-Baptiste Alleaume <jean-baptiste.alleaume@filigran.io>
2025-01-21 08:23:05 +0100

    fix

--- portal-front/src/components/admin/admin-callout.tsx
+++ portal-front/src/components/admin/admin-callout.tsx
@@ -2,28 +2,17 @@
 
 import { Callout } from 'filigran-ui';
 import { useTranslations } from 'next-intl';
-import { useEffect } from 'react';
 import useAdminPath from '../../hooks/useAdminPath';
 
 export function AdminCallout() {
   const t = useTranslations();
   const isAdminPath = useAdminPath();
 
-  useEffect(() => {
-    if (isAdminPath) {
-      document.body.classList.add('!pt-9');
-    } else {
-      document.body.classList.remove('!pt-9');
-    }
-
-    return () => document.body.classList.remove('!pt-9');
-  }, [isAdminPath]);
-
   return (
     isAdminPath && (
       <Callout
         variant="warning"
-        className="absolute z-50 top-0 left-0 right-0 rounded-none !text-black justify-center uppercase">
+        className="rounded-none !text-black justify-center uppercase">
         {t('AdminCallout')}
       </Callout>
     )
