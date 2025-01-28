f18c84b963512955d466f91dfebce8e94d068b1d
Jean-Baptiste Alleaume <jean-baptiste.alleaume@filigran.io>
2025-01-28 09:56:07 +0100

    fix: service-type-badge  and refacto init db test

--- portal-front/src/components/ui/service-type-badge.tsx
+++ portal-front/src/components/ui/service-type-badge.tsx
@@ -3,26 +3,27 @@ import { Badge } from 'filigran-ui';
 import { FunctionComponent } from 'react';
 import { ServiceDefinitionIdentifier } from '../../../__generated__/serviceList_fragment.graphql';
 
-const BADGE_COLORS: Record<ServiceDefinitionIdentifier, string> = {
+type RealServiceDefinitionIdentifier =
+  | Exclude<ServiceDefinitionIdentifier, '%future added value'>
+  | 'default';
+
+const BADGE_COLORS: Record<RealServiceDefinitionIdentifier, string> = {
   vault: 'text-orange',
-  link: 'text-darkblue',
-} as Record<ServiceDefinitionIdentifier, string>;
+  link: 'text-turquoise',
+  default: 'text-gray',
+};
 
 interface ServiceTypeBadgeProps {
   isPending?: boolean;
-  type?: ServiceDefinitionIdentifier;
+  type?: RealServiceDefinitionIdentifier;
   label: string;
 }
 
 export const ServiceTypeBadge: FunctionComponent<ServiceTypeBadgeProps> = ({
   isPending = false,
-  type,
+  type = 'default',
   label,
 }) => {
-  const badgeColor = isPending
-    ? 'text-green'
-    : type
-      ? BADGE_COLORS[type]
-      : 'text-gray';
+  const badgeColor = isPending ? 'text-green' : BADGE_COLORS[type];
   return <Badge className={cn('uppercase', badgeColor)}>{label}</Badge>;
 };
