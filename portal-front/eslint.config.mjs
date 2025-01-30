f4424b25db1d8f3c344045e3d3e5c204a91f0586
Jean-Baptiste Alleaume <jean-baptiste.alleaume@filigran.io>
2025-01-30 09:17:42 +0100

    fix(portal-api): fetch links in loadServiceInstances

--- portal-front/eslint.config.mjs
+++ portal-front/eslint.config.mjs
@@ -1,6 +1,6 @@
+import { FlatCompat } from '@eslint/eslintrc';
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
-import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -22,14 +22,26 @@ const eslintConfig = [
         },
       ],
       'react-hooks/rules-of-hooks': 'off',
-      "@typescript-eslint/ban-ts-comment": ["error", {
-        "ts-expect-error": false
-      }],
-      "@typescript-eslint/no-unused-expressions": ["error", {
-        "allowShortCircuit": true,
-        "allowTernary": true,
-        "allowTaggedTemplates": true
-      }]
+      '@typescript-eslint/ban-ts-comment': [
+        'error',
+        {
+          'ts-expect-error': false,
+        },
+      ],
+      '@typescript-eslint/no-unused-expressions': [
+        'error',
+        {
+          allowShortCircuit: true,
+          allowTernary: true,
+          allowTaggedTemplates: true,
+        },
+      ],
+      'no-console': [
+        'error',
+        {
+          allow: ['warn', 'error'],
+        },
+      ],
     },
   },
 ];
