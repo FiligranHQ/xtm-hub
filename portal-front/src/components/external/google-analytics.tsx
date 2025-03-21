79db48155b22e010e60b11e1f869b9babad5916b
Jean-Baptiste Alleaume <jean-baptiste.alleaume@filigran.io>
2025-03-21 10:51:40 +0100

    fix TS errors

--- portal-front/src/components/external/google-analytics.tsx
+++ portal-front/src/components/external/google-analytics.tsx
@@ -8,8 +8,8 @@ export default function GoogleAnalytics() {
       src="https://www.googletagmanager.com/gtag/js?id=G-9FC0TL0TH3"
       onLoad={() => {
         window.dataLayer = window.dataLayer || [];
-        function gtag() {
-          dataLayer.push(arguments);
+        function gtag(_a: unknown, _b: unknown) {
+          window.dataLayer.push(arguments);
         }
         gtag('js', new Date());
         gtag('config', 'G-9FC0TL0TH3');
