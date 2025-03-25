c78ed99e2e435b91eaf815e6ac17e5c628cd63b2
hervyt <tiffaine.hervy@filigran.io>
2025-03-25 07:59:09 +0100

    Fix UI Design nitpicks #426 #427 #428 (#440)
    * [Frontend] - (Colors): Change menu colors
    
    * [Frontend] - (Spacing): Change submenu spacing

--- portal-front/src/components/menu/menu.tsx
+++ portal-front/src/components/menu/menu.tsx
@@ -33,7 +33,7 @@ export const LinkMenu: FunctionComponent<LinkMenuProps> = ({
           className: 'h-9 w-full justify-start rounded-none normal-case px-m',
         }),
         currentPath === href &&
-          'bg-primary/10 shadow-[inset_2px_0px] shadow-primary'
+          'bg-menu-active shadow-[inset_2px_0px] shadow-primary'
       )}>
       <span className="flex w-8 flex-shrink-0 justify-center">
         <Icon className="h-4 w-4" />
@@ -52,7 +52,7 @@ const Menu = () => {
   return (
     <aside
       className={cn(
-        'mobile:hidden z-20 sticky flex-shrink-0 top-0 left-0 flex h-full flex-col overflow-y-auto overflow-x-hidden bg-page-background duration-300 ease-in-out',
+        'mobile:hidden z-20 sticky flex-shrink-0 top-0 left-0 flex h-full flex-col overflow-y-auto overflow-x-hidden bg-background duration-300 ease-in-out',
         open ? 'w-48' : 'w-14'
       )}>
       <MenuLogo />
