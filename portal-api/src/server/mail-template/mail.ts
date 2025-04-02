8930f45e8a02d6fe7af26dca4f8022a580c20691
Jean-Baptiste Alleaume <jean-baptiste.alleaume@filigran.io>
2025-04-02 15:13:25 +0200

    [backend] feat(emails): send an email to existing users when invited to an organization (#472)

--- portal-api/src/server/mail-template/mail.ts
+++ portal-api/src/server/mail-template/mail.ts
@@ -4,12 +4,18 @@ export interface GenericServiceMailModel {
   serviceName: string;
 }
 export type WelcomeMailModel = Record<string, unknown>;
+export interface NewUserOrganizationMailModel {
+  organizationName: string;
+  userName: string;
+  invitedName: string;
+}
 
 // ATTENTION, the key should be the same as the template file
 export type MailTemplates = {
   welcome: WelcomeMailModel;
   vault: GenericServiceMailModel;
   custom_dashboards: GenericServiceMailModel;
+  new_user_organization: NewUserOrganizationMailModel;
 };
 
 export const templateSubjects: {
@@ -20,4 +26,6 @@ export const templateSubjects: {
     `XTM Hub - You've been invited to the ${params.serviceName}`,
   custom_dashboards: (params: GenericServiceMailModel) =>
     `XTM Hub - You've been invited to the ${params.serviceName}`,
+  new_user_organization: (params: NewUserOrganizationMailModel) =>
+    `XTM Hub - You've been added to the ${params.organizationName} organization`,
 };
