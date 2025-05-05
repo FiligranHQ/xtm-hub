091a868db380fc553b9df2c375672ff4ddfc1d11
Valentin Stern <valentin.stern@filigran.io>
2025-05-05 21:21:28 +0200

    [frontend] feat: add ability to reset password

--- portal-front/src/components/profile/index.tsx
+++ portal-front/src/components/profile/index.tsx
@@ -1,17 +1,46 @@
+'use client';
+
+import { ResetPasswordMutation } from '@/components/me/me.graphql';
 import {
   ProfileFormEdit,
   ProfileFormEditSchema,
 } from '@/components/profile/form/edit';
+import { ProfileFormResetPassword } from '@/components/profile/form/reset-password';
+import { toast } from 'filigran-ui';
+import { useTranslations } from 'next-intl';
 import React from 'react';
+import { useMutation } from 'react-relay';
 
 export const Profile: React.FC = () => {
+  const t = useTranslations();
+  const [commitResetPasswordMutation] = useMutation(ResetPasswordMutation);
+
   const handleSubmit = (values: ProfileFormEditSchema) => {
     console.warn(values);
+  };
+
+  const handleResetPassword = () => {
+    commitResetPasswordMutation({
+      variables: {},
+      onError(error) {
+        toast({
+          variant: 'destructive',
+          title: t('Utils.Error'),
+          description: t(`Error.Server.${error.message}`),
+        });
+      },
+      onCompleted() {
+        toast({
+          title: t('Utils.Success'),
+        });
+      },
+    });
   };
 
   return (
     <>
       <ProfileFormEdit onSubmit={handleSubmit} />
+      <ProfileFormResetPassword onSubmit={handleResetPassword} />
     </>
   );
 };
