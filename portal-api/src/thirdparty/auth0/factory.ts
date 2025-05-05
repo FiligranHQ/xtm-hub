9247177938eea078913fff9c65904156ecd2910d
Valentin Stern <valentin.stern@filigran.io>
2025-05-05 14:20:06 +0200

    [backend] fix: use config variable to enable auth0 service

--- portal-api/src/thirdparty/auth0/factory.ts
+++ portal-api/src/thirdparty/auth0/factory.ts
@@ -1,9 +1,10 @@
+import config from 'config';
 import { Auth0ClientImplementation } from './client';
 import { Auth0ClientMock } from './mock';
 import { Auth0Client } from './type';
 
 export const getAuth0Client = (): Auth0Client => {
-  if (process.env.NODE_ENV === 'test') {
+  if (config.get<boolean>('auth0.mock')) {
     return new Auth0ClientMock();
   }
 
