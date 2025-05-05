dabe939fe6f5cea10e193b2b583b939425f371ff
Valentin Stern <valentin.stern@filigran.io>
2025-05-05 20:51:48 +0200

    [frontend] feat: add basic profile form with initial inputs

--- portal-front/src/components/profile/index.tsx
+++ portal-front/src/components/profile/index.tsx
@@ -1,12 +1,17 @@
-import { useTranslations } from 'next-intl';
+import {
+  ProfileFormEdit,
+  ProfileFormEditSchema,
+} from '@/components/profile/form/edit';
 import React from 'react';
 
 export const Profile: React.FC = () => {
-  const t = useTranslations();
+  const handleSubmit = (values: ProfileFormEditSchema) => {
+    console.warn(values);
+  };
 
   return (
     <>
-      <h1 className="sr-only">{t('ProfilePage.Title')}</h1>
+      <ProfileFormEdit onSubmit={handleSubmit} />
     </>
   );
 };
