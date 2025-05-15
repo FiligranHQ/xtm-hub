37b412115a2de2ea27466e9e721a2b905eb0320f
Valentin Stern <valentin.stern@filigran.io>
2025-05-15 15:41:50 +0200

    [frontend] refactor: improve hook readability

--- portal-front/src/hooks/useIsFeatureEnabled.tsx
+++ portal-front/src/hooks/useIsFeatureEnabled.tsx
@@ -3,8 +3,9 @@ import { useContext } from 'react';
 
 export const useIsFeatureEnabled = (id: string): boolean => {
   const { settings } = useContext(SettingsContext);
+  if (!Array.isArray(settings?.feature_flags)) {
+    return false;
+  }
 
-  return (settings?.feature_flags ?? []).some(
-    (flag) => flag?.id === id && flag.enabled
-  );
+  return settings.feature_flags.some((flag) => flag?.id === id && flag.enabled);
 };
