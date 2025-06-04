854baa76f09afb0743b0c1d3f051719f707fc223
Valentin Stern <valentin.stern@filigran.io>
2025-06-04 17:36:51 +0200

    [e2e] feat: add tests to asserts capabilities for editUser

---
+++ portal-e2e-tests/tests/db-utils/capability.helper.ts
@@ -0,0 +1,24 @@
+import { db } from './db-connection';
+
+export const getUserOrganizationCapabilityNames = async (
+  email: string,
+  organizationName: string
+): Promise<string[]> => {
+  const capabilities = await db('UserOrganization_Capability')
+    .select('UserOrganization_Capability.name')
+    .leftJoin(
+      'User_Organization',
+      'User_Organization.id',
+      'UserOrganization_Capability.user_organization_id'
+    )
+    .leftJoin(
+      'Organization',
+      'Organization.id',
+      'User_Organization.organization_id'
+    )
+    .leftJoin('User', 'User.id', 'User_Organization.user_id')
+    .where('User.email', '=', email)
+    .andWhere('Organization.name', '=', organizationName);
+
+  return Array.from(new Set(capabilities.map(({ name }) => name)));
+};
--- portal-e2e-tests/tests/tests_files/api/user.spec.ts
+++ portal-e2e-tests/tests/tests_files/api/user.spec.ts
@@ -1,6 +1,7 @@
 import { expect, test } from '../../fixtures/baseFixtures';
 import { ApiClient } from './client';
 import gql from 'graphql-tag';
+import { getUserOrganizationCapabilityNames } from '../../db-utils/capability.helper';
 
 test.describe('User API', () => {
   test.describe('editUser', () => {
@@ -26,9 +27,39 @@ test.describe('User API', () => {
       expect(responseBody.includes('Not authorized')).toBeTruthy();
     });
 
-    test('should allow user to update another one when he has MANAGE_ACCESS', async () => {
+    test('should allow user to update another one when he has ADMINISTRATE_ORGANIZATION', async () => {
       const apiClient = await ApiClient.init();
       await apiClient.login('admin@thales.com');
+
+      const capabilities = await getUserOrganizationCapabilityNames(
+        'admin@thales.com',
+        'Thales'
+      );
+
+      expect(capabilities.includes('ADMINISTRATE_ORGANIZATION')).toBeTruthy();
+
+      const thalesUserId = '154006e2-f24b-42da-b39c-e0fb17bead00';
+      const response = await apiClient.callGraphQL(query, {
+        id: thalesUserId,
+        input: {},
+      });
+
+      const responseBody = (await response.body()).toString();
+
+      expect(responseBody.includes('Not authorized')).toBeFalsy();
+    });
+
+    test('should allow user to update another one when he has MANAGE_ACCESS', async () => {
+      const apiClient = await ApiClient.init();
+      await apiClient.login('access-subscription@filigran.io');
+
+      const capabilities = await getUserOrganizationCapabilityNames(
+        'access-subscription@filigran.io',
+        'Filigran'
+      );
+
+      expect(capabilities.includes('MANAGE_ACCESS')).toBeTruthy();
+      expect(capabilities.includes('ADMINISTRATE_ORGANIZATION')).toBeFalsy();
 
       const thalesUserId = '154006e2-f24b-42da-b39c-e0fb17bead00';
       const response = await apiClient.callGraphQL(query, {
