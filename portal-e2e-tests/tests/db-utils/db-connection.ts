2a3ee0c6d43d31f6467032e4ebb4dd5b9ab08770
Valentin <6004961+Kroustille@users.noreply.github.com>
2025-07-02 10:29:03 +0200

    chore(e2e): use migrations and seeds from portal-api (#732)

--- portal-e2e-tests/tests/db-utils/db-connection.ts
+++ portal-e2e-tests/tests/db-utils/db-connection.ts
@@ -1,13 +1,35 @@
 import knex from 'knex';
-import { DB_NAME, DB_PASSWORD, DB_USER } from './const';
+
+const e2eConnection = {
+  user: process.env.POSTGRES_USER,
+  host: process.env.DATABASE_HOST,
+  database: process.env.POSTGRES_DB,
+  password: process.env.POSTGRES_PASSWORD,
+  port: Number(process.env.DATABASE_PORT),
+};
+
+const localConnection = {
+  user: 'portal',
+  host: '127.0.0.1',
+  database: 'test_database',
+  password: 'portal-password',
+  port: 5434,
+};
 
 export const db = knex({
   client: 'pg',
-  connection: {
-    user: DB_USER,
-    host: process.env.E2E_BASE_URL ? process.env.DATABASE_HOST : '127.0.0.1',
-    database: DB_NAME,
-    password: DB_PASSWORD,
-    port: process.env.E2E_BASE_URL ? Number(process.env.DATABASE_PORT) : 5434,
+  connection: process.env.E2E_BASE_URL ? e2eConnection : localConnection,
+  migrations: {
+    extension: 'js',
+    tableName: 'migrations',
+    directory: process.env.MIGRATIONS_PATH
+      ? process.env.MIGRATIONS_PATH
+      : process.cwd() + '/../portal-api/src/migrations',
+  },
+  seeds: {
+    extension: 'js',
+    directory: process.env.SEEDS_PATH
+      ? process.env.SEEDS_PATH
+      : process.cwd() + '/../portal-api/tests/seeds',
   },
 });
