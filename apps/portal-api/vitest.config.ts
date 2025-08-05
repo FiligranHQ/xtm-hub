416d0d1fb95fda178e7964fba2a969f07ceb99b1
Jean-Philippe Kha <jean-philippe.kha@filigran.io>
2025-08-06 00:07:24 +0200

    [frontend/backend] exclude some path for TU coverage

--- apps/portal-api/vitest.config.ts
+++ apps/portal-api/vitest.config.ts
@@ -8,6 +8,17 @@ export default defineConfig({
     fileParallelism: false,
     coverage: {
       provider: 'v8',
+      exclude: [
+        'builder/**',
+        'src/__generated__/**',
+        'src/migrations/**',
+        'tests/**',
+        'config/**',
+        '.kanelrc.js',
+        '**/*.config.*',
+        '**/*.d.ts',
+        '**/*.mjs',
+      ],
     },
   },
   plugins: [], // only if you are using custom tsconfig paths
