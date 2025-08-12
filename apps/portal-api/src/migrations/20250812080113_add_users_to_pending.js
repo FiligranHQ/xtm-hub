da34b3cb1ff91b3a1b5c904ef387888515d3011c
Carine Le Bas <carine.lebas@filigran.io>
2025-08-12 11:04:34 +0200

    [backend] - (pendingUser) add script to add users from domain in pending table #470

---
+++ apps/portal-api/src/migrations/20250812080113_add_users_to_pending.js
@@ -0,0 +1,38 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export async function up(knex) {
+  const userOrganizationData = await knex('User')
+    .select('User.id as user_id', 'org.id as organization_id')
+    .from('User')
+    .join(
+      knex.raw(
+        '"Organization" as org ON split_part("User".email, \'@\', 2) = ANY(org.domains)'
+      )
+    )
+    .whereNotExists(function () {
+      this.select(1)
+        .from('User_Organization as user_org')
+        .whereRaw('user_org.user_id = "User".id')
+        .whereRaw('user_org.organization_id = org.id');
+    })
+    .whereNotExists(function () {
+      this.select(1)
+        .from('User_Organization_Pending as user_org_pending')
+        .whereRaw('user_org_pending.user_id = "User".id')
+        .whereRaw('user_org_pending.organization_id = org.id');
+    });
+
+  if (userOrganizationData.length > 0) {
+    await knex('User_Organization_Pending').insert(userOrganizationData);
+  }
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export async function down(knex) {
+  await knex.delete().from('User_Organization_Pending');
+}
