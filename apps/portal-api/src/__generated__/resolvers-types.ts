cd79d15ba6f7a76ed22904ab4f6fe2fe07913699
Valentin Stern <valentin.stern@filigran.io>
2025-09-01 20:03:45 +0200

    [backend/frontend] feat: add feature flag and new manage OpenAEV registration capability

--- apps/portal-api/src/__generated__/resolvers-types.ts
+++ apps/portal-api/src/__generated__/resolvers-types.ts
@@ -786,6 +786,7 @@ export type OrganizationCapabilitiesInput = {
 export enum OrganizationCapability {
   AdministrateOrganization = 'ADMINISTRATE_ORGANIZATION',
   ManageAccess = 'MANAGE_ACCESS',
+  ManageOpenaevRegistration = 'MANAGE_OPENAEV_REGISTRATION',
   ManageOpenctiRegistration = 'MANAGE_OPENCTI_REGISTRATION',
   ManageSubscription = 'MANAGE_SUBSCRIPTION'
 }
@@ -1169,6 +1170,7 @@ export enum Restriction {
   FrtManageUser = 'FRT_MANAGE_USER',
   FrtServiceSubscriber = 'FRT_SERVICE_SUBSCRIBER',
   ManageAccess = 'MANAGE_ACCESS',
+  ManageOpenaevRegistration = 'MANAGE_OPENAEV_REGISTRATION',
   ManageOpenctiRegistration = 'MANAGE_OPENCTI_REGISTRATION',
   ManageSubscription = 'MANAGE_SUBSCRIPTION'
 }
--- apps/portal-api/src/thirdparty/elasticsearch/README.md
+++ apps/portal-api/src/thirdparty/elasticsearch/README.md
@@ -1,5 +1,3 @@
-
-
 ## How to create a ES DB migration
 
 - Run `yarn esmigrate:make my_modif_name`. It will create a migration file in the src/thirdparty/elasticsearch/migrations.
@@ -7,5 +5,6 @@
 - On you first launch of portal-api, it will update the DB. It will insert a new entry in the ES `migrations` table.
 
 If you want to manually upgrade/downgrade elastic search:
+
 - run `yarn esmigrate:up` to run all migration files
 - run `yarn esmigrate:down` to remove the last migration (it will remove only this one)
