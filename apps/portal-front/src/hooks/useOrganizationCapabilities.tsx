fadacf40537e4bf25674fd73dfde81aee97307be
Valentin <6004961+Kroustille@users.noreply.github.com>
2025-09-04 16:34:25 +0200

    [backend/frontend] feat: manage OpenAEV registration capability #900 (#901)

--- apps/portal-front/src/hooks/useOrganizationCapabilities.tsx
+++ apps/portal-front/src/hooks/useOrganizationCapabilities.tsx
@@ -1,5 +1,15 @@
+import { useIsFeatureEnabled } from '@/hooks/useIsFeatureEnabled';
+import { FeatureFlag } from '@/utils/constant';
 import { OrganizationCapabilityEnum } from '@generated/models/OrganizationCapability.enum';
 
 export const useOrganizationCapabilities = () => {
-  return Object.values(OrganizationCapabilityEnum);
+  const isOpenAEVRegistrationEnabled = useIsFeatureEnabled(
+    FeatureFlag.OPENAEV_REGISTRATION
+  );
+
+  return Object.values(OrganizationCapabilityEnum).filter(
+    (capability) =>
+      capability !== OrganizationCapabilityEnum.MANAGE_OPENAEV_REGISTRATION ||
+      isOpenAEVRegistrationEnabled
+  );
 };
