3fe332396abdd83f74f5b61aea2c3ab90ce0c237
Carine Le Bas <carine.lebas@filigran.io>
2025-09-10 15:32:19 +0200

    fixup! [e2e] - feature(registration): add e2e tests #935

--- apps/portal-e2e-tests/tests/model/register.pageModel.ts
+++ apps/portal-e2e-tests/tests/model/register.pageModel.ts
@@ -1,36 +1,26 @@
 import { Page } from '@playwright/test';
 
+export type PlatformDetails = {
+  url: string;
+  title: string;
+  id: string;
+  contract: string;
+  version?: string;
+};
+
 export default class RegisterPage {
   constructor(private page: Page) {}
 
-  async navigateTo(
-    redirection_key: string,
-    platformUrl: string,
-    platformTitle: string,
-    platformId: string,
-    platformContract: string,
-    platformVersion?: string
-  ) {
-    const url = `/redirect/${redirection_key}?platform_url=${platformUrl}&platform_title=${platformTitle}&platform_id=${platformId}&platform_contract=${platformContract}&platform_version=${platformVersion}`;
+  async navigateTo(redirectionKey: string, platformDetails: PlatformDetails) {
+    const url = `/redirect/${redirectionKey}?platform_url=${platformDetails.url}&platform_title=${platformDetails.title}&platform_id=${platformDetails.id}&platform_contract=${platformDetails.contract}&platform_version=${platformDetails.version}`;
     await this.page.goto(encodeURI(url));
   }
 
   async navigateToAndRegister(
-    redirection_key: string,
-    platformUrl: string,
-    platformTitle: string,
-    platformId: string,
-    platformContract: string,
-    platformVersion?: string
+    redirectionKey: string,
+    platformDetails: PlatformDetails
   ) {
-    await this.navigateTo(
-      redirection_key,
-      platformUrl,
-      platformTitle,
-      platformId,
-      platformContract,
-      platformVersion
-    );
+    await this.navigateTo(redirectionKey, platformDetails);
     await this.page.getByRole('button', { name: 'Register' }).click();
   }
 }
--- apps/portal-e2e-tests/tests/tests_files/registration.spec.ts
+++ apps/portal-e2e-tests/tests/tests_files/registration.spec.ts
@@ -1,11 +1,10 @@
 import { expect, test } from '../fixtures/baseFixtures';
 import LoginPage from '../model/login.pageModel';
-import RegisterPage from '../model/register.pageModel';
+import RegisterPage, { PlatformDetails } from '../model/register.pageModel';
 import { HomePage } from '../model/home.pageModel';
 import { waitForReactIdle } from '../model/common';
-import { SERVICE_NAME } from '../model/dashboard.pageModel';
 
-const OPENAEV_PLATFORM_URL = {
+const OPENAEV_PLATFORM_URL: PlatformDetails = {
   url: 'http://localhost:3000',
   title: 'Open%20AEV%20Instance',
   id: '916121bf-d246-4a43-8522-24be19537b91',
@@ -13,7 +12,7 @@ const OPENAEV_PLATFORM_URL = {
   version: '1.0.0',
 };
 
-const OPENCTI_PLATFORM_URL = {
+const OPENCTI_PLATFORM_URL: PlatformDetails = {
   url: 'http://localhost:3000',
   title: 'Open%20CTI%20Instance',
   id: '916121bf-d246-4a43-8522-24be19537b91',
@@ -33,14 +32,7 @@ test.describe('Registration', () => {
   test('should register openAEV platform', async ({ page }) => {
     await test.step('display register page', async () => {
       registerPage = new RegisterPage(page);
-      await registerPage.navigateTo(
-        'register-openaev',
-        OPENAEV_PLATFORM_URL.url,
-        OPENAEV_PLATFORM_URL.title,
-        OPENAEV_PLATFORM_URL.id,
-        OPENAEV_PLATFORM_URL.contract,
-        OPENAEV_PLATFORM_URL.version
-      );
+      await registerPage.navigateTo('register-openaev', OPENAEV_PLATFORM_URL);
       await expect(
         page.getByRole('heading', {
           name: "You're about to register your OpenAEV platform into XTM Hub",
@@ -66,13 +58,7 @@ test.describe('Registration', () => {
   test('should register openCTI platform', async ({ page }) => {
     await test.step('display register page', async () => {
       registerPage = new RegisterPage(page);
-      await registerPage.navigateTo(
-        'register-opencti',
-        OPENCTI_PLATFORM_URL.url,
-        OPENCTI_PLATFORM_URL.title,
-        OPENCTI_PLATFORM_URL.id,
-        OPENCTI_PLATFORM_URL.contract
-      );
+      await registerPage.navigateTo('register-opencti', OPENCTI_PLATFORM_URL);
       await expect(
         page.getByRole('heading', {
           name: "You're about to register your OpenCTI platform into XTM Hub",
