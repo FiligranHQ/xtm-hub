732fea08bafe8ba4f95a1ea2eec04da68b9c9fe0
thervy <tiffaine.hervy@filigran.io>
2025-09-10 11:52:31 +0200

    [frontend] - (Connectors): Fix conflicts #322

--- apps/portal-front/src/components/connectors/connector-contract-card.tsx
+++ apps/portal-front/src/components/connectors/connector-contract-card.tsx
@@ -19,32 +19,29 @@ const ConnectorContractCard: FunctionComponent<ConnectorContractCardProps> = ({
   contract,
   version = 'master',
 }) => {
-  const t = useTranslations();
-
   return (
     <li className="overflow-hidden border-light flex flex-col relative rounded border bg-page-background hover:bg-hover">
-      <div className="flex items-stretch gap-l p-l">
-        <div className="w-24 self-stretch flex">
-          <img
-            src={contract.logo}
-            alt={`${contract.title} logo`}
-            className="rounded w-full h-full object-contain"
-          />
-        </div>
-
-        <div className="flex-1 min-w-0">
-          <div className="flex items-center gap-2">
-            <h2 className="text-base md:text-lg font-semibold leading-tight min-w-0">
-              <a
-                href={`/opencti-connectors/master/${contract.slug}`}
-                className="block truncate">
-                {contract.title}
-              </a>
-            </h2>
-            {contract.verified && (
-              <VerifiedIcon className="h-6 w-6 shrink-0 text-green-500" />
-            )}
+      <a
+        className="flex flex-col h-full"
+        href={`/cybersecurity-solutions/opencti-connectors/${version}/${contract.slug}`}>
+        <div className="flex items-stretch gap-l p-l">
+          <div className="w-24 self-stretch flex">
+            <img
+              src={contract.logo}
+              alt={`${contract.title} logo`}
+              className="rounded w-full h-full object-contain"
+            />
           </div>
+
+          <div className="flex-1 min-w-0">
+            <div className="flex items-center gap-2">
+              <h2 className="text-base md:text-lg font-semibold leading-tight min-w-0">
+                {contract.title}
+              </h2>
+              {contract.verified && (
+                <VerifiedIcon className="h-6 w-6 shrink-0 text-green-500" />
+              )}
+            </div>
 
             <div className="mt-s flex flex-wrap gap-s mb-xs">
               <BadgeOverflowCounter
