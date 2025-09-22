7f8e4ff447273d8a1ed4739725216601a31602c3
jpkha <jean-philippe.kha@filigran.io>
2025-09-22 12:15:00 +0200

    [frontend] Modify connector card #958 (#960)
    [frontend] Update connector card #958

--- apps/portal-front/src/components/connectors/service-instance-connector-card.tsx
+++ apps/portal-front/src/components/connectors/service-instance-connector-card.tsx
@@ -1,34 +1,74 @@
-import ServiceInstanceCard from '@/components/service/service-instance-card';
 import { FeatureFlag } from '@/utils/constant';
+import { PUBLIC_CYBERSECURITY_SOLUTIONS_PATH } from '@/utils/path/constant';
 import { isFeatureEnabled } from '@/utils/settings.service';
-import { ServiceDefinitionIdentifierEnum } from '@generated/models/ServiceDefinitionIdentifier.enum';
-import { ServiceInstanceCreationStatusEnum } from '@generated/models/ServiceInstanceCreationStatus.enum';
+import { LogoFiligranIcon } from 'filigran-icon';
+import { AspectRatio } from 'filigran-ui/servers';
 import { getTranslations } from 'next-intl/server';
+import Image from 'next/image';
+import Link from 'next/link';
 
 export const ServiceInstanceConnectorCard = async () => {
   const isConnectorsPageEnabled = await isFeatureEnabled(
     FeatureFlag.CONNECTORS_PAGE
   );
   const t = await getTranslations();
+
+  const connectorServiceInstance = {
+    name: t('Service.Connectors.Name'),
+    slug: 'opencti-connectors',
+    description: t('Service.Connectors.Description'),
+    illustration_document: '/integrations_library.png',
+    logo_document: '/octi_connectors.png',
+  };
+
+  const serviceHref = `/${PUBLIC_CYBERSECURITY_SOLUTIONS_PATH}/${connectorServiceInstance.slug}`;
+
   return (
     <>
       {isConnectorsPageEnabled && (
-        <ServiceInstanceCard
-          key={'Connector'}
-          serviceInstance={{
-            id: 'Connector',
-            creation_status: ServiceInstanceCreationStatusEnum.CREATED,
-            name: t('Service.Connectors.Name'),
-            slug: 'opencti-connectors',
-            description: t('Service.Connectors.Description'),
-            illustration_document_id: '' as string,
-            logo_document_id: '' as string,
-            service_definition_identifier:
-              ServiceDefinitionIdentifierEnum.CSV_FEEDS,
-            ordering: 0,
-          }}
-          seo={true}
-        />
+        <li
+          key="Connector"
+          className="relative border border-light rounded flex">
+          <div className="z-[2] flex-1 overflow-hidden relative group focus-within:ring-2 focus-within:ring-ring rounded flex flex-col">
+            <div className="flex relative justify-center items-center flex-col gap-s overflow-hidden box-border px-s bg-blue-900">
+              <LogoFiligranIcon className="absolute text-white opacity-[0.03] z-1 size-60 rotate-45 -translate-x-24 -translate-y-12" />
+              <div className="mt-s flex items-center h-12 w-full">
+                <div
+                  className="w-full h-12"
+                  style={{
+                    backgroundImage: `url(${connectorServiceInstance.logo_document})`,
+                    backgroundSize: 'contain',
+                    backgroundPosition: 'left center',
+                    backgroundRepeat: 'no-repeat',
+                  }}
+                />
+              </div>
+              <AspectRatio
+                ratio={16 / 9}
+                className="rounded-t overflow-hidden">
+                <Image
+                  fill
+                  src={connectorServiceInstance.illustration_document}
+                  objectPosition="top"
+                  objectFit="cover"
+                  alt={`Illustration of ${connectorServiceInstance.name}`}
+                />
+              </AspectRatio>
+            </div>
+            <div className="min-h-40 flex flex-col p-l gap-l flex-1 bg-page-background group-hover:bg-hover">
+              <div className="flex items-start min-h-12 w-full text-ellipsis overflow-hidden">
+                <Link
+                  href={serviceHref}
+                  className="focus-visible:outline-none after:cursor-pointer after:content-[' '] after:absolute after:inset-0 z-0">
+                  <h2>{connectorServiceInstance.name}</h2>
+                </Link>
+              </div>
+              <p className="txt-sub-content text-muted-foreground">
+                {connectorServiceInstance.description}
+              </p>
+            </div>
+          </div>
+        </li>
       )}
     </>
   );
