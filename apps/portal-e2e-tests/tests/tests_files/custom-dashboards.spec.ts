1db2439a5c3db3154cd4cf6816c8c63451d5d59c
jpkha <jean-philippe.kha@filigran.io>
2025-09-25 11:14:54 +0200

    [e2e] skip dashboard public page (#1015)

--- apps/portal-e2e-tests/tests/tests_files/custom-dashboards.spec.ts
+++ apps/portal-e2e-tests/tests/tests_files/custom-dashboards.spec.ts
@@ -49,7 +49,9 @@ test.describe('Custom dashboards', () => {
     ).toBeVisible();
   });
 
-  test('Should see the custom dashboard on public page', async ({ page }) => {
+  test.skip('Should see the custom dashboard on public page', async ({
+    page,
+  }) => {
     await page.goto('/');
     await expect(page).toHaveScreenshot();
     await dashboardPage.navigateToPublicCustomDashboard();
