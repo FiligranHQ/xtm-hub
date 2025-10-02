f46ce2b0c91e17fd62a082443f80d8f82dc673d6
Jean-Baptiste Alleaume <jean-baptiste.alleaume@filigran.io>
2025-10-02 08:35:56 +0200

    [frontend] improve message for one-click deploy when no registered platform

--- apps/portal-front/src/components/service/document/one-click-deploy/no-platform-display.tsx
+++ apps/portal-front/src/components/service/document/one-click-deploy/no-platform-display.tsx
@@ -1,6 +1,7 @@
 import { Button } from 'filigran-ui/servers';
 import { useTranslations } from 'next-intl';
 import Image from 'next/image';
+import Markdown from 'react-markdown';
 
 interface NoPlatformDisplayProps {
   setIsOpen: (isOpen: boolean) => void;
@@ -20,11 +21,13 @@ const NoPlatformDisplay = ({
             platformIdentifier,
           })}
         </h1>
-        <p>
+          <Markdown>
           {t(
-            'Service.ShareableResources.Deploy.DeployDescriptionNoPlatformThen'
-          )}
-        </p>
+            'Service.ShareableResources.Deploy.DeployDescriptionNoPlatformThen', {
+              platformIdentifier,
+            }
+              )}
+          </Markdown>
         <div className="relative border-2 border-solid rounded w-full h-96">
           <Image
             fill
