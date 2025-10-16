f6e5ae502a02dce99357041e93ced49781b2c1db
Valentin Stern <valentin.stern@filigran.io>
2025-10-16 10:14:29 +0200

    [backend] test: assert subscription is deleted

---
+++ apps/portal-api/src/modules/subcription/subscription.app.test.ts
@@ -0,0 +1,34 @@
+import { v4 as uuidv4 } from 'uuid';
+import { describe, expect, it } from 'vitest';
+import {
+  contextAdminUser,
+  FILIGRAN_ORGA_ID,
+  SERVICE_CSV_FEEDS_ID,
+} from '../../../tests/tests.const';
+import { SubscriptionId } from '../../model/kanel/public/Subscription';
+import { subscriptionApp } from './subscription.app';
+import { createSubscription } from './subscription.domain';
+
+describe('Subscription app', () => {
+  describe(`${subscriptionApp.deleteSubscription.name}`, () => {
+    it('should delete the subscription', async () => {
+      const id = uuidv4() as SubscriptionId;
+      const subscription = await createSubscription(contextAdminUser, {
+        id,
+        organization_id: FILIGRAN_ORGA_ID,
+        service_instance_id: SERVICE_CSV_FEEDS_ID,
+        start_date: new Date(),
+        end_date: null,
+        status: 'ACCEPTED',
+        joining: 'AUTO_JOIN',
+        billing: 0,
+        justification: null,
+      });
+
+      expect(subscription).toBeDefined();
+
+      const deletedSubscription = await subscriptionApp.deleteSubscription(id);
+      expect(deletedSubscription).toStrictEqual(subscription);
+    });
+  });
+});
