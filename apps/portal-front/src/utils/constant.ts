6f34bd72b08c002430a8b78678028f210cfbc4d9
jpkha <jean-philippe.kha@filigran.io>
2025-10-24 18:46:32 +0200

    [frontend] clean and remove old connectors public path #1123 (#1170)

--- apps/portal-front/src/utils/constant.ts
+++ apps/portal-front/src/utils/constant.ts
@@ -5,6 +5,5 @@ export enum FeatureFlag {
   // dummy feature flag used for testing purposes
   DUMMY = 'DUMMY',
   OPENAEV_REGISTRATION = 'OPENAEV_REGISTRATION',
-  CONNECTORS_PAGE = 'CONNECTORS_PAGE',
   OPEN_CTI_FREE_TRIAL = 'OPEN_CTI_FREE_TRIAL',
 }
