8cb479e4f51fa6c4bf8ff8f8cf80b3023131b67b
Carine Le Bas <carine.lebas@filigran.io>
2025-10-24 09:24:46 +0200

    [backend] - feature(databaseContext): create databaseContext to better manage transactions #1168

---
+++ apps/portal-api/src/databaseContext.ts
@@ -0,0 +1,44 @@
+import { AsyncLocalStorage } from 'async_hooks';
+import { Knex } from 'knex';
+import { database } from '../knexfile';
+
+interface DatabaseContext {
+  transaction?: Knex.Transaction;
+}
+
+const dbAsyncLocalStorage = new AsyncLocalStorage<DatabaseContext>();
+
+export const databaseContext = {
+  withTransaction: async <T>(callback: () => Promise<T>): Promise<T> => {
+    // If we're already in a transaction context, reuse it
+    const existingContext = dbAsyncLocalStorage.getStore();
+    if (existingContext?.transaction) {
+      return callback();
+    }
+
+    // Create a new transaction
+    return database.transaction(async (trx) => {
+      const context: DatabaseContext = { transaction: trx };
+
+      return dbAsyncLocalStorage.run(context, async () => {
+        const result = await callback();
+        return result;
+      });
+    });
+  },
+
+  getTransaction: (): Knex.Transaction | undefined => {
+    return dbAsyncLocalStorage.getStore()?.transaction;
+  },
+
+  clearTransaction: (): void => {
+    const context = dbAsyncLocalStorage.getStore();
+    if (context) {
+      delete context.transaction;
+    }
+  },
+
+  isInTransaction: (): boolean => {
+    return !!dbAsyncLocalStorage.getStore()?.transaction;
+  },
+};
