f28b48bd45cc7c67b6fa2603292cacc9b4bb3eae
jpkha <jean-philippe.kha@filigran.io>
2025-10-24 09:44:11 +0200

    add logo connector in public path (#1167)
    [frontend] add logo connector in public path

--- apps/portal-front/src/components/service/document/connector/shareable-resource-connector-slug-public.tsx
+++ apps/portal-front/src/components/service/document/connector/shareable-resource-connector-slug-public.tsx
@@ -9,6 +9,8 @@ import { Merge } from '@/utils/typescript';
 import { seoIntegrationFeedConnectorFragment$data } from '@generated/seoIntegrationFeedConnectorFragment.graphql';
 import { seoIntegrationFeedFragment$data } from '@generated/seoIntegrationFeedFragment.graphql';
 import { VerifiedIcon } from 'filigran-icon';
+import Image from 'next/image';
+import React from 'react';
 import { MarkdownAsync } from 'react-markdown';
 
 export type ShareableResourceConnectorType = Merge<
@@ -18,35 +20,51 @@ export type ShareableResourceConnectorType = Merge<
 interface ShareableResourceConnectorSlugPublicProps {
   documentData: ShareableResourceConnectorType;
   pageUrl: string;
+  logo: string;
 }
 
 const ShareableResourceConnectorSlugPublic: React.FunctionComponent<
   ShareableResourceConnectorSlugPublicProps
-> = ({ documentData, pageUrl }) => {
+> = ({ documentData, logo, pageUrl }) => {
   return (
     <>
       <div className="flex gap-s pb-l flex-col md:flex-row">
-        <h1 className="whitespace-nowrap">{documentData.name}</h1>
-        {documentData.verified && (
-          <div className="flex items-center gap-s py-xs px-l font-semibold bg-green-100  text-green-500 dark:bg-turquoise-900 rounded-lg">
-            <VerifiedIcon className="h-5 w-5 shrink-0 mr-xs" />
-            Verified
-          </div>
-        )}
-        <div className="flex gap-s overflow-hidden flex-1 items-center">
-          <BadgeOverflowCounter
-            badges={documentData?.labels as BadgeOverflow[]}
-            className="z-[2]"
+        <div className="w-24 flex-shrink-0 rounded overflow-hidden">
+          <Image
+            src={logo}
+            alt={`${documentData.name} logo`}
+            width={96}
+            height={96}
+            loading="lazy"
+            className="w-full h-full object-contain rounded"
           />
         </div>
-        <div className="flex items-center gap-2 ml-auto">
-          <ShareLinkButton
-            documentId={documentData.id}
-            url={pageUrl}
-            tooltipText={`Service.OpenctiIntegrationFeeds.Actions.Share`}
-          />
+        <div className="flex flex-col flex-1 justify-center">
+          <div className="flex items-center gap-s flex-wrap">
+            <h1 className="whitespace-nowrap">{documentData.name}</h1>
+            {documentData.verified && (
+              <div className="flex items-center gap-s py-xs px-l font-semibold bg-green-100  text-green-500 dark:bg-turquoise-900 rounded-lg">
+                <VerifiedIcon className="h-5 w-5 shrink-0 mr-xs" />
+                Verified
+              </div>
+            )}
+            <div className=" ml-auto">
+              <ShareLinkButton
+                documentId={documentData.id}
+                url={pageUrl}
+                tooltipText={`Service.OpenctiIntegrationFeeds.Actions.Share`}
+              />
+            </div>
+          </div>
+          <div className="w-full mt-s mb-xs">
+            <BadgeOverflowCounter
+              badges={documentData?.labels as BadgeOverflow[]}
+              className="z-[2]"
+            />
+          </div>
         </div>
       </div>
+
       <div className="flex flex-col-reverse lg:flex-row w-full mt-l gap-xl">
         <div className="flex-[3_3_0%]">
           <h3 className="py-s txt-container-title truncate text-muted-foreground">
