d51c0bbea19fbf49966a0a27a109897d21b37d94
thervy <tiffaine.hervy@filigran.io>
2025-10-29 08:39:06 +0100

    [frontend] - (Free trial): Add Feature Flag #796

--- apps/portal-front/src/components/service/home/owned-services.tsx
+++ apps/portal-front/src/components/service/home/owned-services.tsx
@@ -1,6 +1,8 @@
 'use client';
 
 import SkeletonServiceCard from '@/components/service/home/skeleton-service-card';
+import { useIsFeatureEnabled } from '@/hooks/useIsFeatureEnabled';
+import { FeatureFlag } from '@/utils/constant';
 import {
   hasTrialInstance,
   publicServiceInstanceToInstanceCardData,
@@ -25,6 +27,9 @@ const OwnedServices = ({
   publicServices,
   registeredPlatforms,
 }: OwnedServicesProps) => {
+  const isFreeTrialFeatureEnabled = useIsFeatureEnabled(
+    FeatureFlag.OPEN_CTI_FREE_TRIAL
+  );
   // Merge and sort by ordering property
   const sortedServices = [
     ...services
@@ -43,9 +48,12 @@ const OwnedServices = ({
     return (
       <Suspense>
         <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-4 gap-l">
-          {!hasTrialInstance(
-            registeredPlatforms.map(registeredPlatformToServiceInstanceCardData)
-          ) && <SkeletonServiceCard />}
+          {isFreeTrialFeatureEnabled &&
+            !hasTrialInstance(
+              registeredPlatforms.map(
+                registeredPlatformToServiceInstanceCardData
+              )
+            ) && <SkeletonServiceCard />}
           {sortedServices.map((service) => (
             <ServiceInstanceCard
               key={service.id}
