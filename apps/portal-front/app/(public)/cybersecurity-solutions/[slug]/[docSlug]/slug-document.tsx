41d210f31736995288ac61aebe12962c50cd4d11
Valentin <6004961+Kroustille@users.noreply.github.com>
2025-11-03 11:04:27 +0100

    [backend] fix: allow unauthenticated user to access public data (#1211)

--- apps/portal-front/app/(public)/cybersecurity-solutions/[slug]/[docSlug]/slug-document.tsx
+++ apps/portal-front/app/(public)/cybersecurity-solutions/[slug]/[docSlug]/slug-document.tsx
@@ -18,14 +18,16 @@ const SlugDocument = async ({
   switch (serviceInstance.slug as ServiceSlug) {
     case ServiceSlug.OPEN_CTI_INTEGRATION_FEEDS:
       return (
-        <div className="relative w-full h-[35vh]">
-          <Image
-            fill
-            className="object-cover object-top rounded-lg"
-            src={`/document/images/${serviceInstance.id}/${document.children_documents![0]!.id}`}
-            alt={`A picture of ${document.name}`}
-          />
-        </div>
+        document.children_documents![0] && (
+          <div className="relative w-full h-[35vh]">
+            <Image
+              fill
+              className="object-cover object-top rounded-lg"
+              src={`/document/images/${serviceInstance.id}/${document.children_documents![0]!.id}`}
+              alt={`A picture of ${document.name}`}
+            />
+          </div>
+        )
       );
 
     case ServiceSlug.OPEN_CTI_CUSTOM_DASHBOARDS:
