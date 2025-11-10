65ddc62d43a6896f6f80a53dfa4048ad09bad1ad
Carine Le Bas <carine.lebas@filigran.io>
2025-11-10 17:43:45 +0100

    [backend] refacto(portalContext): remove portalContext from services definition and capability signature #1225

--- apps/portal-api/src/modules/services/definition/service-definition.domain.ts
+++ apps/portal-api/src/modules/services/definition/service-definition.domain.ts
@@ -1,6 +1,5 @@
 import { db } from '../../../../knexfile';
 import { PlatformIdentifier } from '../../../__generated__/resolvers-types';
-import { requestContext } from '../../../context/request.context';
 import ServiceDefinition, {
   ServiceDefinitionMutator,
 } from '../../../model/kanel/public/ServiceDefinition';
@@ -10,11 +9,7 @@ export const serviceDefinitionDomain = {
   loadServiceDefinitionBy(
     field: ServiceDefinitionMutator
   ): Promise<ServiceDefinition> {
-    const context = requestContext.require();
-    return db(context.portalContext, 'ServiceDefinition')
-      .where(field)
-      .select('id')
-      .first();
+    return db('ServiceDefinition').where(field).select('id').first();
   },
 
   loadServiceDefinitionByPlatformIdentifier(
