99a8eb767c9ee026785faa3d06f46efbad9b8c9a
Jean-Philippe Kha <jean-philippe.kha@filigran.io>
2025-11-27 15:19:31 +0100

    [backend] init ServiceGroup_User and ServiceGroup DB #1300

---
+++ apps/portal-api/src/migrations/20251127141209_add_group_groupUser.js
@@ -0,0 +1,48 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export async function up(knex) {
+  return knex.schema
+    .createTable('ServiceGroup', function (table) {
+      table.uuid('id').primary().defaultTo(knex.raw('gen_random_uuid()'));
+      table.string('name').notNullable();
+      table.uuid('service_instance_id').notNullable();
+      table
+        .foreign('service_instance_id')
+        .references('id')
+        .inTable('ServiceInstance')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE');
+    })
+    .createTable('ServiceGroup_User', function (table) {
+      table.uuid('group_id').notNullable();
+      table.uuid('user_id').notNullable();
+
+      table
+        .foreign('group_id')
+        .references('id')
+        .inTable('ServiceGroup')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE');
+
+      table
+        .foreign('user_id')
+        .references('id')
+        .inTable('User')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE');
+
+      // Composite unique constraint to prevent duplicate user-group pairs
+      table.unique(['group_id', 'user_id']);
+    });
+}
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export async function down(knex) {
+  return knex.schema
+    .dropTableIfExists('ServiceGroup_User')
+    .dropTableIfExists('ServiceGroup');
+}
