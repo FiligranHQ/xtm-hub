82680dfb9cf5468fdea5568b9b8c49e2a986f9ce
Valentin Stern <valentin.stern@filigran.io>
2025-12-11 13:39:50 +0100

    [backend] chore: add deployment request quota table

---
+++ apps/portal-api/src/migrations/20251211105051_add-deployment-request-quotas.js
@@ -0,0 +1,52 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export async function up(knex) {
+  await knex.schema.createTable('DeploymentRequestQuota', (table) => {
+    table.uuid('id', { primary: true }).defaultTo(knex.fn.uuid());
+    table.string('region');
+    table.string('platform_identifier');
+    table.integer('capacity');
+    table.integer('availability');
+
+    table.unique(['region', 'platform_identifier']);
+  });
+
+  const values = [
+    {
+      region: 'apac_au',
+      capacity: 10,
+      availability: 10,
+      platform_identifier: 'opencti',
+    },
+    {
+      region: 'apac_sg',
+      capacity: 10,
+      availability: 10,
+      platform_identifier: 'opencti',
+    },
+    {
+      region: 'eu_west',
+      capacity: 20,
+      availability: 20,
+      platform_identifier: 'opencti',
+    },
+    {
+      region: 'us_east',
+      capacity: 20,
+      availability: 20,
+      platform_identifier: 'opencti',
+    },
+  ];
+
+  await knex('DeploymentRequestQuota').insert(values);
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export async function down(knex) {
+  await knex.schema.dropTable('DeploymentRequestQuota');
+}
