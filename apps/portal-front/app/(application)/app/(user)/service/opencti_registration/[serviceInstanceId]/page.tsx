0a2347655e7b3f67c2051799ee5cab938c612721
Jean-Philippe Kha <jean-philippe.kha@filigran.io>
2025-12-12 09:27:22 +0100

    [frontend] fix issue between client and server component

--- apps/portal-front/app/(application)/app/(user)/service/opencti_registration/[serviceInstanceId]/page.tsx
+++ apps/portal-front/app/(application)/app/(user)/service/opencti_registration/[serviceInstanceId]/page.tsx
@@ -1,63 +1,14 @@
-'use client';
+import { TrialsLearnMore } from '@/components/service/trial-instances/trials-learn-more';
+import ClientPage from './client-page';
 
-import {
-  registeredPlatformByServiceInstanceId,
-  registeredPlatformByServiceInstanceIdFragment,
-} from '@/components/registration/register/register.graphql';
-import { TrialsDetailsPage } from '@/components/service/trial-instances/trials-details-page';
-import { BreadcrumbNav } from '@/components/ui/breadcrumb-nav';
-import { APP_PATH } from '@/utils/path/constant';
-import { PlatformContractEnum } from '@generated/models/PlatformContract.enum';
-import { registeredPlatformByServiceInstanceId_fragment$key } from '@generated/registeredPlatformByServiceInstanceId_fragment.graphql';
-import { registeredPlatformByServiceInstanceIdQuery } from '@generated/registeredPlatformByServiceInstanceIdQuery.graphql';
-import { notFound } from 'next/navigation';
-import { use } from 'react';
-import { useFragment, useLazyLoadQuery } from 'react-relay';
-
-interface ServiceOpenCTIRegistrationPageProps {
+export interface ServiceOpenCTIRegistrationPageProps {
   params: Promise<{ serviceInstanceId: string }>;
 }
-
 const Page = ({ params }: ServiceOpenCTIRegistrationPageProps) => {
-  const { serviceInstanceId } = use(params);
-  const decodedServiceInstanceId = decodeURIComponent(serviceInstanceId);
-
-  const queryData =
-    useLazyLoadQuery<registeredPlatformByServiceInstanceIdQuery>(
-      registeredPlatformByServiceInstanceId,
-      {
-        input: {
-          service_instance_id: decodedServiceInstanceId,
-        },
-      }
-    );
-
-  const data = useFragment<registeredPlatformByServiceInstanceId_fragment$key>(
-    registeredPlatformByServiceInstanceIdFragment,
-    queryData.registeredPlatform
-  );
-
-  if (!data || data.contract !== PlatformContractEnum.TRIAL) {
-    notFound();
-  }
-
-  const breadcrumbs = [
-    {
-      label: 'MenuLinks.Home',
-      href: `/${APP_PATH}`,
-    },
-    {
-      label: data.title,
-      original: true,
-    },
-  ];
-
   return (
     <>
-      <BreadcrumbNav value={breadcrumbs} />
-      {data.contract === PlatformContractEnum.TRIAL && (
-        <TrialsDetailsPage platform={data} />
-      )}
+      <ClientPage params={params} />
+      <TrialsLearnMore />
     </>
   );
 };
--- apps/portal-front/src/components/service/trial-instances/trials-details-page.tsx
+++ apps/portal-front/src/components/service/trial-instances/trials-details-page.tsx
@@ -1,9 +1,8 @@
-import { TrialDetails } from '@/components/service/trial-instances/trial-details';
 import { TrialsHeader } from '@/components/service/trial-instances/trials-header';
-import { TrialsLearnMore } from '@/components/service/trial-instances/trials-learn-more';
 import { registeredPlatformByServiceInstanceId_fragment$data } from '@generated/registeredPlatformByServiceInstanceId_fragment.graphql';
 import React from 'react';
 import { ContactUsButton } from './contact-us-button';
+import { TrialDetails } from './trial-details';
 
 interface Props {
   platform: registeredPlatformByServiceInstanceId_fragment$data;
@@ -14,7 +13,6 @@ export const TrialsDetailsPage: React.FC<Props> = ({ platform }) => {
     <>
       <TrialsHeader actions={<ContactUsButton variant="gradient" />} />
       <TrialDetails platform={platform} />
-      <TrialsLearnMore />
     </>
   );
 };
