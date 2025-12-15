3a530c5af5fc280a719450fce8439ecabe1b5b8a
Valentin Stern <valentin.stern@filigran.io>
2025-12-15 15:54:22 +0100

    chore: use production cron schedule

--- apps/portal-api/src/crons.ts
+++ apps/portal-api/src/crons.ts
@@ -16,7 +16,5 @@ const expireTrials = async (): Promise<void> => {
 
 export const initCronJobs = () => {
   logApp.info('Initializing cron jobs');
-  // cron.schedule('0 2 * * *', expireTrials);
-  // For test purposes
-  cron.schedule('* * * * *', expireTrials);
+  cron.schedule('0 2 * * *', expireTrials);
 };
