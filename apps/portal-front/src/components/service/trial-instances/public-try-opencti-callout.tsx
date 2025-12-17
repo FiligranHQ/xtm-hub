4486e4cdf3327ce35dfc90e7f456209a81c25257
jpkha <jean-philippe.kha@filigran.io>
2025-12-17 15:27:19 +0100

    [frontend] switch banner issue #1407 (#1413)
    * [frontend] switch banner issue #1407
    
    * [frontend] switch banner issue #1407
    
    * [frontend] modify public banner

--- apps/portal-front/src/components/service/trial-instances/public-try-opencti-callout.tsx
+++ apps/portal-front/src/components/service/trial-instances/public-try-opencti-callout.tsx
@@ -9,12 +9,12 @@ export function PublicTryOpenCTICallout() {
   return (
     <Callout
       variant="destructive"
-      className="rounded-none from-blue to-turquoise-300 bg-gradient-to-r text-black justify-center uppercase">
+      className="rounded-none from-blue to-turquoise-300 bg-gradient-to-r text-black justify-center">
       <div>
         {t('Service.Trials.Explore')}
         <Link
-          href={`/redirect/free-trial`}
-          className="ml-xs underline">
+          href={`/cybersecurity-solutions/free-trial`}
+          className="ml-xs underline font-bold">
           {t('Service.Trials.LearnMore')}
         </Link>
       </div>
