5d32eeb9558279c89aa4c849ded0ad32af270c0a
Carine Le Bas <carine.lebas@filigran.io>
2025-12-18 18:03:35 +0100

    refactor(service-capability): move editServiceCapability to app and add tests #1423

--- apps/portal-api/src/modules/user_service/service-capability/service-capability.resolver.ts
+++ apps/portal-api/src/modules/user_service/service-capability/service-capability.resolver.ts
@@ -1,45 +1,21 @@
 import { Resolvers } from '../../../__generated__/resolvers-types';
-import { withTransaction } from '../../../context/database.context';
 import { ServiceInstanceId } from '../../../model/kanel/public/ServiceInstance';
 import { UserServiceId } from '../../../model/kanel/public/UserService';
-import { assertUserCanManageService } from '../../../security/guard';
 import { UnknownErrorCode } from '../../../utils/error/error.code';
 import { mapToGraphQLError } from '../../../utils/error/error.mapping';
 import { extractId } from '../../../utils/utils';
-import { fillSubscriptionWithOrgaServiceAndUserService } from '../../subcription/subscription.domain';
-import { insertCapabilities } from '../user-service-capability/user-service-capability.helper';
-import {
-  deleteUserCapabilityById,
-  loadUserServiceById,
-} from '../user_service.domain';
-import { willManageAccessBeConserved } from './service_capability.helper';
+import { serviceCapabilityApp } from './service-capability.app';
 
 const resolvers: Resolvers = {
   Mutation: {
-    editServiceCapability: async (
-      _,
-      { input, serviceInstanceId },
-      portalContext
-    ) => {
+    editServiceCapability: async (_, { input, serviceInstanceId }) => {
       try {
         const user_service_id = extractId<UserServiceId>(input.user_service_id);
 
-        await assertUserCanManageService(
-          portalContext.user,
-          serviceInstanceId as ServiceInstanceId
-        );
-        await willManageAccessBeConserved(user_service_id, input.capabilities);
-
-        const userService = await withTransaction(async () => {
-          await deleteUserCapabilityById(user_service_id);
-
-          const userService = await loadUserServiceById(user_service_id);
-
-          await insertCapabilities(input.capabilities, userService);
-          return userService;
-        });
-        return fillSubscriptionWithOrgaServiceAndUserService(
-          userService.subscription_id
+        return serviceCapabilityApp.editServiceCapability(
+          user_service_id,
+          input.capabilities,
+          extractId<ServiceInstanceId>(serviceInstanceId)
         );
       } catch (error) {
         throw mapToGraphQLError(error, UnknownErrorCode.EditCapabilitiesError);
