8761ff60f39a89c733d940df996ae7606aeb44af
Tiffaine HERVY <tiffaine.hervy@filigran.io>
2026-01-26 16:20:44 +0100

    [frontend] - (Integration) Add documentation link #1563

--- apps/portal-front/src/components/service/document/shareable-resouce-details.tsx
+++ apps/portal-front/src/components/service/document/shareable-resouce-details.tsx
@@ -1,6 +1,7 @@
 import { formatDate } from '@/utils/date';
 import { LogoFiligranIcon } from '@filigran/icon';
 import * as React from 'react';
+import { useMemo } from 'react';
 
 import { Avatar } from '@filigran/ui/clients';
 
@@ -15,9 +16,10 @@ import {
   ShareableResource,
 } from '@/utils/shareable-resources/shareable-resources.types';
 import { docHasMetadata } from '@/utils/shareable-resources/utils/shareable-resources.client.utils';
-import { Badge } from '@filigran/ui/servers';
+import { Badge, Button } from '@filigran/ui/servers';
+import { IntegrationTypeEnum } from '@generated/models/IntegrationType.enum';
 import { useTranslations } from 'next-intl';
-import { useMemo } from 'react';
+import Link from 'next/link';
 
 // Component interface
 interface ShareableResourceDetailsProps {
@@ -25,6 +27,19 @@ interface ShareableResourceDetailsProps {
   downloadNumber?: number;
 }
 
+const DOCUMENTATION_URLS: Record<string, string | null> = {
+  [IntegrationTypeEnum.CSV_FEED]:
+    'https://docs.opencti.io/latest/usage/import/csv-feed/',
+  [IntegrationTypeEnum.THIRD_PARTY_INTEGRATION]: null,
+  [IntegrationTypeEnum.STREAM]:
+    'https://docs.opencti.io/latest/usage/import/internal-streams/',
+  [IntegrationTypeEnum.TAXII_FEED]:
+    'https://docs.opencti.io/latest/usage/import/taxii-feed/',
+  [IntegrationTypeEnum.RSS_FEED]: null,
+  [IntegrationTypeEnum.JSON_FEED]: null,
+  [IntegrationTypeEnum.CONNECTOR]:
+    'https://docs.opencti.io/latest/usage/import/external-connectors/',
+};
 const ShareableResourceDetails: React.FunctionComponent<
   ShareableResourceDetailsProps
 > = ({ documentData, downloadNumber }) => {
@@ -37,6 +52,9 @@ const ShareableResourceDetails: React.FunctionComponent<
 
     return getIntegrationSubTypeMetadata(documentData.integration_subtype);
   }, [isIntegration, documentData]);
+  const documentationUrl =
+    isIntegrationItem(documentData) &&
+    DOCUMENTATION_URLS[documentData.integration_type];
 
   return (
     <ShareableResourceBasicInformation>
@@ -121,6 +139,23 @@ const ShareableResourceDetails: React.FunctionComponent<
         <ShareableResourceDetailItem
           label={t('Service.ShareableResources.Details.ProductVersion')}>
           <span>{documentData.product_version}</span>
+        </ShareableResourceDetailItem>
+      )}
+      {documentationUrl && (
+        <ShareableResourceDetailItem
+          label={t('Service.ShareableResources.Details.Documentation')}>
+          <Button
+            className="p-0"
+            variant="link"
+            asChild>
+            <Link
+              href={documentationUrl}
+              rel="noopener noreferrer"
+              target="_blank">
+              <OpenInNewIcon className="h-4 w-4 mr-s" />
+              {documentationUrl}
+            </Link>
+          </Button>
         </ShareableResourceDetailItem>
       )}
       <ShareableResourceDetailItem
