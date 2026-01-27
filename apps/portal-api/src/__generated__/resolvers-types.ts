64a5ad94c7d329eb0b703daf9f9f4185ba6e3fcf
Jean-Baptiste Alleaume <jean-baptiste.alleaume@filigran.io>
2026-01-27 10:43:17 +0100

    [backend/frontend] fix(integrations-library): filter platforms to only show active trials

--- apps/portal-api/src/__generated__/resolvers-types.ts
+++ apps/portal-api/src/__generated__/resolvers-types.ts
@@ -1536,6 +1536,7 @@ export type RegisteredPlatformInput = {
 
 export type RegisteredPlatformsInput = {
   identifier?: InputMaybe<PlatformIdentifier>;
+  onlyActiveTrials?: InputMaybe<Scalars['Boolean']['input']>;
 };
 
 export type RegistrationResponse = {
