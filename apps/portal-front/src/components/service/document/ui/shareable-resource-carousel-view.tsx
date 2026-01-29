d40343b2a38983a7c5b9b651e78763e818b5f23d
hervyt <tiffaine.hervy@filigran.io>
2026-01-29 15:39:57 +0100

    [frontend] - (OpenAEV Scenarios) Add images in carousel #1539 (#1584)
    * [frontend] - (OpenAEV Scenarios) Add images in carousel #1539
    
    * [frontend] - (UPdateScenario) Fix Tooltip #1539

---
+++ apps/portal-front/src/components/service/document/ui/shareable-resource-carousel-view.tsx
@@ -0,0 +1,77 @@
+'use client';
+import { Carousel, CarouselItem, DialogContent } from '@filigran/ui/clients';
+import * as React from 'react';
+import { useState } from 'react';
+
+import { Dialog } from '@filigran/ui';
+import { customDashboardsItem_fragment$data } from '@generated/customDashboardsItem_fragment.graphql';
+import { openaevScenariosItem_fragment$data } from '@generated/openaevScenariosItem_fragment.graphql';
+import { serviceInstance_fragment$data } from '@generated/serviceInstance_fragment.graphql';
+import Image from 'next/image';
+
+// Component interface
+interface ShareableResourceCarouselProps {
+  documentData:
+    | customDashboardsItem_fragment$data
+    | openaevScenariosItem_fragment$data;
+  serviceInstance: serviceInstance_fragment$data;
+}
+
+const ShareableResourceCarousel: React.FunctionComponent<
+  ShareableResourceCarouselProps
+> = ({ documentData, serviceInstance }) => {
+  const [open, setOpen] = useState<boolean>(false);
+  const [pictureIndex, setPictureIndex] = useState<number>(0);
+  const fileNames = (documentData.children_documents ?? []).map(
+    (doc) => doc?.id
+  );
+  const handleCarouselImageClick = (open: boolean, index: number) => {
+    setOpen(open);
+    setPictureIndex(index);
+  };
+  return (
+    <>
+      {fileNames.length > 0 && (
+        <Carousel className="h-[35vh]">
+          {fileNames.map((name, index) => (
+            <CarouselItem
+              key={name}
+              className="cursor-pointer"
+              onClick={() => handleCarouselImageClick(true, index)}>
+              <Image
+                fill
+                objectFit="cover"
+                objectPosition="top"
+                src={`/document/images/${serviceInstance.id}/${name}`}
+                alt={`A picture of ${name}`}
+              />
+            </CarouselItem>
+          ))}
+          <Dialog
+            open={open}
+            onOpenChange={setOpen}>
+            <DialogContent className="max-h-[calc(100dvh)] h-screen w-screen max-w-[calc(100dvw)] ">
+              <Carousel
+                opts={{
+                  startIndex: pictureIndex,
+                }}>
+                {fileNames.map((name) => (
+                  <CarouselItem key={name}>
+                    <Image
+                      fill
+                      objectFit="contain"
+                      src={`/document/images/${serviceInstance.id}/${name}`}
+                      alt={`A picture of ${name}`}
+                    />
+                  </CarouselItem>
+                ))}
+              </Carousel>
+            </DialogContent>
+          </Dialog>
+        </Carousel>
+      )}
+    </>
+  );
+};
+
+export default ShareableResourceCarousel;
