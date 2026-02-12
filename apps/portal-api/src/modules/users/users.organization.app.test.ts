775c7494d1041f140255309ecdb1ed96fecb18dd
jpkha <jean-philippe.kha@filigran.io>
2026-02-12 17:13:32 +0100

    [backend] fix pending user list #1672 (#1687)

--- apps/portal-api/src/modules/users/users.organization.app.test.ts
+++ apps/portal-api/src/modules/users/users.organization.app.test.ts
@@ -1,11 +1,15 @@
 import { v4 as uuidv4 } from 'uuid';
 import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest';
+import { db } from '../../../knexfile';
 import { TEST_ORGANIZATIONS } from '../../../tests/tests.const';
 import portalConfig from '../../config';
 import User, { UserId } from '../../model/kanel/public/User';
+import UserOrganizationPending from '../../model/kanel/public/UserOrganizationPending';
 import * as MailService from '../../server/mail-service';
+import { loadUserOrganization } from '../common/user-organization.domain';
 import { UserOrganizationPendingDomain } from './users-pending/user-organization-pending.domain';
 import { UsersOrganizationApp } from './users.organization.app';
+import { insertUser } from './users.test.utils';
 
 describe('UsersOrganizationApp', () => {
   describe('sendPendingUsersDigest', () => {
@@ -212,6 +216,89 @@ describe('UsersOrganizationApp', () => {
       await UsersOrganizationApp.sendPendingUsersDigest();
 
       expect(sendMailSpy).not.toHaveBeenCalled();
+    });
+
+    it('should not send mail if user is already in organization and remove from pending list', async () => {
+      portalConfig.enabled_emails = {
+        ...originalEnabledEmails,
+        pending_user_digest: true,
+      };
+      const newUser = await insertUser({
+        email: 'testPendingUser@filigran.io',
+      });
+
+      await db<UserOrganizationPending>('User_Organization').insert({
+        user_id: newUser.id,
+        organization_id: TEST_ORGANIZATIONS.FILIGRAN.ID,
+      });
+      await UserOrganizationPendingDomain.insertNewUserOrganizationPending({
+        user_id: newUser.id,
+        organization_id: TEST_ORGANIZATIONS.FILIGRAN.ID,
+      });
+
+      const userPendingExisted =
+        await UserOrganizationPendingDomain.loadUserOrganizationPending({
+          user_id: newUser.id,
+          organization_id: TEST_ORGANIZATIONS.FILIGRAN.ID,
+        });
+
+      expect(userPendingExisted.length).toEqual(1);
+
+      const sendMailSpy = vi.spyOn(MailService, 'sendMail');
+      await UsersOrganizationApp.sendPendingUsersDigest();
+      expect(sendMailSpy).not.toHaveBeenCalled();
+
+      const userPendingShouldExistAnymore =
+        await UserOrganizationPendingDomain.loadUserOrganizationPending({
+          user_id: newUser.id,
+          organization_id: TEST_ORGANIZATIONS.FILIGRAN.ID,
+        });
+
+      expect(userPendingShouldExistAnymore.length).toEqual(0);
+    });
+  });
+  describe('addUserToOrganization', async () => {
+    it('should add user and remove user from pending list', async () => {
+      const newUser = await insertUser({
+        email: 'testAddingPendingUser@filigran.io',
+      });
+      await UserOrganizationPendingDomain.insertNewUserOrganizationPending({
+        user_id: newUser.id,
+        organization_id: TEST_ORGANIZATIONS.FILIGRAN.ID,
+      });
+
+      const userShouldNotBeInTheOrg = await loadUserOrganization({
+        user_id: newUser.id,
+        organization_id: TEST_ORGANIZATIONS.FILIGRAN.ID,
+      });
+
+      expect(userShouldNotBeInTheOrg.length).toEqual(0);
+
+      const userPendingExisted =
+        await UserOrganizationPendingDomain.loadUserOrganizationPending({
+          user_id: newUser.id,
+          organization_id: TEST_ORGANIZATIONS.FILIGRAN.ID,
+        });
+
+      expect(userPendingExisted.length).toEqual(1);
+      await UsersOrganizationApp.addUserToOrganization({
+        email: 'testAddingPendingUser@filigran.io',
+      });
+
+      const userPendingShouldExistAnymore =
+        await UserOrganizationPendingDomain.loadUserOrganizationPending({
+          user_id: newUser.id,
+          organization_id: TEST_ORGANIZATIONS.FILIGRAN.ID,
+        });
+
+      expect(userPendingShouldExistAnymore.length).toEqual(0);
+
+      const userShouldBeAdded = await loadUserOrganization({
+        user_id: newUser.id,
+        organization_id: TEST_ORGANIZATIONS.FILIGRAN.ID,
+      });
+
+      expect(userShouldBeAdded.length).toEqual(1);
     });
   });
 });
