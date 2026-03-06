f49aca732873782eb338d92c2ff51393718bb2be
Valentin Stern <valentin.stern@filigran.io>
2026-03-06 15:56:03 +0100

    [frontend] fix(integrations): hide download button for third party integrations

--- apps/portal-front/src/components/service/document/shareable-resouce-details.tsx
+++ apps/portal-front/src/components/service/document/shareable-resouce-details.tsx
@@ -12,7 +12,10 @@ import { getIntegrationSubTypeMetadata } from '@/components/service/integrations
 import { roundToNearest } from '@/lib/utils';
 import { formatPersonNames } from '@/utils/format/name';
 import { isIntegrationItem } from '@/utils/shareable-resources/shareable-resources.types';
-import { docHasMetadata } from '@/utils/shareable-resources/utils/shareable-resources.client.utils';
+import {
+  docHasMetadata,
+  isResourceDownloadable,
+} from '@/utils/shareable-resources/utils/shareable-resources.client.utils';
 import { Badge } from '@filigran/ui/servers';
 import { documentItem_fragment$data } from '@generated/documentItem_fragment.graphql';
 import { IntegrationTypeEnum } from '@generated/models/IntegrationType.enum';
@@ -147,10 +150,12 @@ const ShareableResourceDetails: React.FunctionComponent<
           <ShareableResourceDetailsLink url={documentationUrl} />
         </ShareableResourceDetailItem>
       )}
-      <ShareableResourceDetailItem
-        label={t('Service.ShareableResources.Details.Downloads')}>
-        <span>{roundToNearest(downloadNumber)}</span>
-      </ShareableResourceDetailItem>
+      {isResourceDownloadable(documentData) && (
+        <ShareableResourceDetailItem
+          label={t('Service.ShareableResources.Details.Downloads')}>
+          <span>{roundToNearest(downloadNumber)}</span>
+        </ShareableResourceDetailItem>
+      )}
       <ShareableResourceDetailItem
         label={t('Service.ShareableResources.Details.Shares')}>
         <span>{roundToNearest(documentData.share_number)}</span>
