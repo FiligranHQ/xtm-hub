18c3b94d92068e0381e96e0f3d8a8b8e384095cb
Carine Le Bas <carine.lebas@filigran.io>
2026-03-13 10:45:35 +0100

    [backend] chore(competitors): fix kanel generated file, with enum in model and correct primary key #1850

--- apps/portal-api/.kanelrc.js
+++ apps/portal-api/.kanelrc.js
@@ -1,3 +1,18 @@
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const { defaultGetPropertyMetadata } = require('kanel');
+
+/**
+ * Maps "TableName.columnName" to the TypeScript enum type that should replace `string`.
+ * The importPath is used as-is in the generated import statement (isAbsolute: true),
+ * so it must be relative to the generated file location (src/model/kanel/public/).
+ */
+const COLUMN_ENUM_MAP = {
+  'Competitor.tier': {
+    tsType: 'CompetitorTier',
+    importPath: '../../../__generated__/resolvers-types',
+  },
+};
+
 /** @type {import('kanel').Config} */
 module.exports = {
   connection: {
@@ -10,9 +25,30 @@ module.exports = {
 
   preDeleteOutputFolder: true,
   outputPath: './src/model/kanel',
+  schemas: ['public'],
 
   customTypeMap: {
     'pg_catalog.tsvector': 'string',
     'pg_catalog.bpchar': 'string',
+  },
+
+  getPropertyMetadata(property, details, generateFor, config) {
+    const mapping = COLUMN_ENUM_MAP[`${details.name}.${property.name}`];
+    if (mapping) {
+      return {
+        name: property.name,
+        typeOverride: {
+          name: mapping.tsType,
+          typeImports: [{
+            name: mapping.tsType,
+            isDefault: false,
+            path: mapping.importPath,
+            isAbsolute: true,
+            importAsType: true,
+          }],
+        },
+      };
+    }
+    return defaultGetPropertyMetadata(property, details, generateFor, config);
   },
 };
---
+++ apps/portal-api/src/migrations/20260313000000_fix_competitor_id_primary_key.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export async function up(knex) {
+  await knex.schema.alterTable('Competitor', (table) => {
+    table.primary(['id']);
+  });
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export async function down(knex) {
+  await knex.schema.alterTable('Competitor', (table) => {
+    table.dropPrimary();
+  });
+}
