d892e828b1f528a5cf01d4edc36356c3062f28c3
Tiffaine HERVY <tiffaine.hervy@filigran.io>
2026-04-09 14:11:15 +0200

    [backend] chore(Tests): Remove contextAdminByDefault and fix service-instance.domain.test (#2066)

--- apps/portal-api/src/modules/services/service-instance.domain.test.ts
+++ apps/portal-api/src/modules/services/service-instance.domain.test.ts
@@ -12,6 +12,7 @@ import {
 import { TestHelper } from '../../../tests/test.helper';
 import {
   contextRegistererUserSecondOrga,
+  requestContextAdminUser,
   SERVICES,
   TEST_ORGANIZATIONS,
 } from '../../../tests/tests.const';
@@ -21,6 +22,7 @@ import {
   ServiceDefinitionIdentifier,
   ServiceInstanceTag,
 } from '../../__generated__/resolvers-types';
+import { requestContext } from '../../context/request.context';
 import ServiceInstance, {
   ServiceInstanceId,
 } from '../../model/kanel/public/ServiceInstance';
@@ -595,6 +597,9 @@ describe('Service instance domain', () => {
     `(
       'should return subscriptions for searchTerm=$searchTerm',
       async ({ searchTerm, expectedNames }) => {
+        // Given
+        requestContext.set(requestContextAdminUser);
+
         // When
         const result = await loadServiceWithSubscriptions(
           serviceInstanceId,
