efe5976305736eb96d141221a7ed3049279d4693
Julien Gérard <julien.gerard@filigran.io>
2026-04-13 17:05:22 +0200

    [frontend] refactor(avatar): add object-cover on avatar in shareables

--- apps/portal-front/src/components/service/document/shareable-resouce-details.tsx
+++ apps/portal-front/src/components/service/document/shareable-resouce-details.tsx
@@ -83,7 +83,7 @@ const ShareableResourceDetails: React.FunctionComponent<
       <ShareableResourceDetailItem
         label={t('Service.ShareableResources.Details.Author')}>
         <div className="flex items-center gap-s">
-          <div className="size-8">
+          <div className="size-8 [&_img]:object-cover">
             <Avatar src={documentData.uploader?.picture ?? ''} />
           </div>
           <span>{formatPersonNames(documentData.uploader)}</span>
