65018ae65f1748176521ec92ad6dcc984e15d767
hervyt <tiffaine.hervy@filigran.io>
2026-04-21 17:14:12 +0200

    [frontend] feat(EpicRoadmap): Change placeholder (#2190) (#2198)

--- apps/portal-front/src/components/epic/epic-filter.tsx
+++ apps/portal-front/src/components/epic/epic-filter.tsx
@@ -45,7 +45,7 @@ export const EpicFilter = ({
     <div className="ml-xl pl-m grid grid-cols-1 sm:grid-cols-3 gap-l items-center">
       <div className="max-w-full sm:max-w-[100%]">
         <SearchInput
-          placeholder={t('Epic.Search')}
+          placeholder={t('GenericActions.Search')}
           onChange={debounceHandleInput}
         />
       </div>
