e5737e8c1ddb72b559a39b49a4a9d9f75c03bd5c
Julien Gérard <julien.gerard@filigran.io>
2026-04-21 18:07:35 +0200

    [frontend] fix(chatbot):update chatbot

--- apps/portal-front/app/(application)/app/layout.tsx
+++ apps/portal-front/app/(application)/app/layout.tsx
@@ -2,7 +2,6 @@ import * as React from 'react';
 import { FunctionComponent } from 'react';
 
 import '@filigran/chatbot/styles.css';
-import '@filigran/ui/theme.css';
 import '../../../styles/globals.css';
 
 import serverPortalApiFetch from '@/relay/serverPortalApiFetch';
