ee71be101f7aeea106c04b14abd68fb98f39dc82
Julien Gérard <julien.gerard@filigran.io>
2026-05-13 10:40:19 +0200

    [frontend] delete outdated file

--- apps/frontend/src/i18n/navigation.ts
+++
@@ -1,9 +0,0 @@
-import { defaultLocale, locales } from '@/i18n/config';
-import { createNavigation } from 'next-intl/navigation';
-
-export const { Link, redirect, usePathname, useRouter, getPathname } =
-  createNavigation({
-    locales,
-    defaultLocale,
-    localePrefix: 'always',
-  });
