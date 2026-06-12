f40ec4fd2f1144dbc9f4fb96b5b9f1dfaaaa91c8
hervyt <tiffaine.hervy@filigran.io>
2026-06-12 09:27:50 +0200

    feat(manageserviceaccess): add the button for bypass user (#2527) (#2550)
    * [frontend] feat(ManageServiceAccess): Add the button for bypass user (#2527)
    
    * refactor(test): refactor test
    
    ---------
    
    Co-authored-by: Jean-Philippe Kha <jean-philippe.kha@filigran.io>

--- apps/frontend/src/components/service/components/header/ServiceListHeaderButtons.tsx
+++ apps/frontend/src/components/service/components/header/ServiceListHeaderButtons.tsx
@@ -2,6 +2,7 @@ import { PortalContext } from '@/components/me/AppPortalContext';
 import { ServiceListIntegrationDropdown } from '@/components/service/components/header/ServiceListIntegrationDropdown';
 import { useServiceContext } from '@/components/service/components/ServiceContext';
 import { ServiceManageSheet } from '@/components/service/components/ServiceManageSheet';
+import { useAdminByPass } from '@/hooks/use-portal-capability';
 import useServiceCapability from '@/hooks/use-service-capability';
 import { APP_PATH } from '@/utils/path/constant';
 import { ShareableResourceType } from '@/utils/shareable-resources/shareable-resources.types';
@@ -24,6 +25,7 @@ const ServiceListHeaderButtons = ({}) => {
     currentUserSubscriptionId,
   } = useServiceContext();
   const [openSheet, setOpenSheet] = useState(false);
+  const isBypass = useAdminByPass();
 
   const canManageService =
     serviceInstance.capabilities.includes(
@@ -44,7 +46,7 @@ const ServiceListHeaderButtons = ({}) => {
 
   return (
     <div className="flex gap-s">
-      {canManageService && (
+      {(canManageService || isBypass) && currentUserSubscriptionId && (
         <>
           <Button
             asChild
