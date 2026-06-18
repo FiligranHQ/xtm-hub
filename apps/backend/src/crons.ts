c6d28bde38070b0d952c1a60e79e4fa89997999d
Antoine Quintin <antoine.quintin@filigran.io>
2026-06-18 16:02:14 +0200

    refactor(roadmap): enforce roadmapLink template type and narrow reminder cron to the 23rd-25th (#2554)

--- apps/backend/src/crons.ts
+++ apps/backend/src/crons.ts
@@ -65,7 +65,7 @@ export const initCronJobs = () => {
   scheduledTasks.push(cron.schedule('0 2 * * *', expireTrials));
   scheduledTasks.push(cron.schedule('0 9 * * 1', sendPendingUserDigest));
   scheduledTasks.push(
-    cron.schedule('0 8 * * 1-5', sendPublicRoadmapMonthlyReminder, {
+    cron.schedule('0 8 23-25 * *', sendPublicRoadmapMonthlyReminder, {
       timezone: 'Europe/Paris',
     })
   );
--- apps/backend/src/server/mail-template/mail.ts
+++ apps/backend/src/server/mail-template/mail.ts
@@ -116,8 +116,7 @@ export type MailTemplates = {
   free_trial_user_added: FreeTrialUserAddedModel;
   organization_pending_user_digest: OrganizationPendingUserDigestModel;
   admin_saas_instance_requested: AdminSaasInstanceRequestedModel;
-  // eslint-disable-next-line @typescript-eslint/no-empty-object-type
-  public_roadmap_monthly_reminder: {};
+  public_roadmap_monthly_reminder: { roadmapLink: string };
 };
 
 export const templateSubjects: {
