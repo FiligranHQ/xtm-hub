52904c3a0fee9ff67c06623d86d199d46a8a9640
Valentin <6004961+Kroustille@users.noreply.github.com>
2026-07-01 12:08:24 +0200

    refactor(token-directive): simplify validation logic and update tests (#2702) (#2725)
    * refactor(token-direction): simplify validation logic and update tests
    
    * chore: implement copilot review comments

--- apps/backend/src/modules/security-management/token/platform-token.util.ts
+++ apps/backend/src/modules/security-management/token/platform-token.util.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import { PlatformConfigurationStatus } from '../../../__generated__/resolvers-types';
+import type DeploymentRequest from '../../../model/kanel/public/DeploymentRequest';
 import { logApp } from '../../../utils/app-logger.util';
 import { DeploymentRequestDomain } from '../../deployment/deployment.domain';
 import { PlatformConfigurationDomain } from '../../registration/platform-configuration/platform-configuration.domain';
@@ -17,29 +18,28 @@ export const extractPlatformId = (req: express.Request): string | null => {
 
 export const validateExistsPlatformAndToken = (
   req: express.Request
-): boolean => {
+): { token: string; platform_id: string } | null => {
   const token = extractPlatformToken(req);
   if (!token) {
     logApp.warn('[validatePlatformToken] Missing platformToken header');
-    return false;
+    return null;
   }
 
-  const platformId = extractPlatformId(req);
-  if (!platformId) {
+  const platform_id = extractPlatformId(req);
+  if (!platform_id) {
     logApp.warn('[validatePlatformToken] Missing platformId header');
-    return false;
+    return null;
   }
-  return true;
+  return { token, platform_id };
 };
 
 export const validateActivePlatformToken = async (
   req: express.Request
 ): Promise<boolean> => {
-  if (!validateExistsPlatformAndToken(req)) return false;
-  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-  const platform_id = extractPlatformId(req)!;
-  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-  const token = extractPlatformToken(req)!;
+  const extractedAuth = validateExistsPlatformAndToken(req);
+  if (!extractedAuth) return false;
+
+  const { platform_id, token } = extractedAuth;
 
   const platformConfiguration =
     await PlatformConfigurationDomain.loadConfigurationByPlatformAndToken({
@@ -50,27 +50,27 @@ export const validateActivePlatformToken = async (
   return platformConfiguration?.status === PlatformConfigurationStatus.Active;
 };
 
-export const validateAndGetRequestedPlatformToken = async (
-  req: express.Request
-) => {
-  if (!validateExistsPlatformAndToken(req)) return null;
-  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-  const platform_token = extractPlatformToken(req)!;
-
+export const validateAndGetRequestedPlatformToken = async ({
+  platform_id,
+  token,
+}: {
+  platform_id: string;
+  token: string;
+}): Promise<DeploymentRequest | undefined> => {
   const deploymentRequest =
     await DeploymentRequestDomain.loadDeploymentRequestBy({
-      platform_token,
+      platform_token: token,
     });
 
   const isValid =
     deploymentRequest &&
     deploymentRequest.platform_id !== null &&
-    deploymentRequest.platform_id === extractPlatformId(req);
+    deploymentRequest.platform_id === platform_id;
   if (!isValid) {
     logApp.warn(
       '[validatePlatformToken] No registration matching token, or invalid platformId provided'
     );
   }
 
-  return isValid ? deploymentRequest : null;
+  return isValid ? deploymentRequest : undefined;
 };
