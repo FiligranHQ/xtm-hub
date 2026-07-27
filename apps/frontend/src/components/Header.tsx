e09a6c7aa90c0e7fad4f036894d52a0d68b103ba
Carine Le Bas <carine.lebas@filigran.io>
2026-07-27 14:31:00 +0200

    feat(homepage): fix mobile menu image and double close icon  (#2958)

--- apps/frontend/src/components/Header.tsx
+++ apps/frontend/src/components/Header.tsx
@@ -11,11 +11,10 @@ import { IconActions, IconActionsItem } from '@/components/ui/IconActions';
 import { cn } from '@/lib/utils';
 import { APP_PATH } from '@/utils/path/constant';
 
-import { CloseIcon, MenuIcon } from '@filigran/icon';
+import { MenuIcon } from '@filigran/icon';
 import {
   Avatar,
   Sheet,
-  SheetClose,
   SheetContent,
   SheetHeader,
   SheetTitle,
@@ -25,7 +24,6 @@ import { useTranslations } from 'next-intl';
 import Link from 'next/link';
 
 import { OrganizationCapability } from '@graphql/generated';
-import Logo from '@public/logo.svg';
 import { usePathname, useRouter } from 'next/navigation';
 import { useContext, useEffect, useState } from 'react';
 import { useMutation } from 'react-relay';
@@ -114,20 +112,11 @@ const HeaderComponent = ({ displayLogo }: HeaderComponentProps) => {
           <SheetContent side="left">
             <SheetHeader className="flex flex-row justify-between pl-l">
               <div className="flex items-center gap-s">
-                <Logo
-                  className="h-8 w-8"
-                  aria-hidden={true}
-                />
-                <SheetTitle>{t('Header.BrandName')}</SheetTitle>
+                <DisplayLogo className="text-primary h-8" />
+                <SheetTitle className="sr-only">
+                  {t('Header.BrandName')}
+                </SheetTitle>
               </div>
-              <SheetClose>
-                <CloseIcon
-                  aria-hidden={true}
-                  focusable={false}
-                  className="h-4 w-4 mr-xl"
-                />
-                <span className="sr-only">{t('Header.CloseMenu')}</span>
-              </SheetClose>
             </SheetHeader>
             <div className="flex flex-1 flex-col h-full justify-between">
               <div className="pt-m flex flex-col gap-m px-m">
